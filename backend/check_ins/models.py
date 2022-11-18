ba27f5e126492c887ad32a3ec3b1cd0e6a9822b7
Christian Beckham <urenacb22@gmail.com>
2022-11-18 15:18:43 -0600

    feat: created check_ins app, created CheckIn model, created CheckInSerializer, and created urls.py file

---
+++ backend/check_ins/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+
+
+class CheckIn(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=4, decimal_places=1)
+    feedback = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # need to add image field to upload/store images
---
+++ backend/check_ins/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import CheckIn
+
+
+class CheckInSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CheckIn
+        fields = ('user_id', 'id', 'weight', 'feedback', 'created_date')
