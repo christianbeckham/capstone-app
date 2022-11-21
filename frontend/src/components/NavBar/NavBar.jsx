101f794a99b708a0a99bd23370404c548fe0be1f
Christian Beckham <urenacb22@gmail.com>
2022-11-21 12:25:24 -0600

    refactor: deleted NavBar.css styles and updated NavBar component to use Material UI components

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,29 +2,67 @@ import React, { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 
-import "./NavBar.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import WifiTethering from "@mui/icons-material/WifiTethering";
 
 const Navbar = () => {
 	const { logoutUser, user } = useContext(AuthContext);
 	const navigate = useNavigate();
 
 	return (
-		<div className="navBar">
-			<ul>
-				<li className="brand">
-					<Link to="/" style={{ textDecoration: "none", color: "white" }}>
-						<b>React/Django JWT App</b>
-					</Link>
-				</li>
-				<li>
+		<Box sx={{ flexGrow: 1, margin: 0 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<WifiTethering sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+					<Typography
+						variant="h6"
+						noWrap
+						component={Link}
+						to="/"
+						sx={{
+							width: 100,
+							mr: 2,
+							display: { xs: "none", md: "flex" },
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "inherit",
+							textDecoration: "none",
+							flexGrow: 1,
+						}}
+					>
+						FitHub
+					</Typography>
 					{user ? (
-						<button onClick={logoutUser}>Logout</button>
+						<Button
+							color="inherit"
+							onClick={logoutUser}
+							startIcon={
+								<Avatar sx={{ width: 24, height: 24, bgcolor: "green" }}>
+									{user.username[0]}
+								</Avatar>
+							}
+						>
+							Logout
+						</Button>
 					) : (
-						<button onClick={() => navigate("/login")}>Login</button>
+						<Button
+							onClick={() => navigate("/login")}
+							color="inherit"
+							startIcon={<AccountCircle />}
+						>
+							Sign in
+						</Button>
 					)}
-				</li>
-			</ul>
-		</div>
+				</Toolbar>
+			</AppBar>
+		</Box>
 	);
 };
 
