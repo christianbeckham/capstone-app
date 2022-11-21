444d8345dc625ba00b028e498740f010ad34bcaf
Christian Beckham <urenacb22@gmail.com>
2022-11-21 09:57:31 -0600

    feat: added endpoint urls to urlpatterns and created view functions for CRUD functionality

--- backend/client_requests/urls.py
+++ backend/client_requests/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('/'),
+    path('', views.requests_list),
+    path('<int:pk>/', views.requests_detail),
+    path('all/', views.admin_requests_list),
 ]
--- backend/client_requests/views.py
+++ backend/client_requests/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from .models import ClientRequest
+from .serializers import ClientRequestSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def admin_requests_list(request):
+    if request.method == 'GET':
+        requests = ClientRequest.objects.all()
+        serializer = ClientRequestSerializer(requests, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def requests_list(request):
+    if request.method == 'GET':
+        requests = get_list_or_404(ClientRequest, user_id=request.user.id)
+        serializer = ClientRequestSerializer(requests, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ClientRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def requests_detail(request, pk):
+    req = get_object_or_404(ClientRequest, pk=pk, user_id=request.user.id)
+
+    if request.method == 'GET':
+        serializer = ClientRequestSerializer(req)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = ClientRequestSerializer(req, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        req.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
