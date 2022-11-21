eced83706c8dfa1ad77ffd22f1f905adcd2fa5db
Christian Beckham <urenacb22@gmail.com>
2022-11-21 12:28:00 -0600

    refactor: updated pages to use new form component

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,52 +1,14 @@
-import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
-import useCustomForm from "../../hooks/useCustomForm";
-
-import "./LoginPage.css";
+import React from "react";
+import Container from "@mui/material/Container";
+import SignInForm from "../../components/SignInForm/SignInForm";
 
 const LoginPage = () => {
-	const defaultValues = { username: "", password: "" };
-	const { loginUser, isServerError } = useContext(AuthContext);
-	const [formData, handleInputChange, handleSubmit, reset] = useCustomForm(
-		defaultValues,
-		loginUser
-	);
-
-	useEffect(() => {
-		if (isServerError) {
-			reset();
-		}
-	}, [isServerError]);
-
 	return (
-		<div className="container">
-			<form className="form" onSubmit={handleSubmit}>
-				<label>
-					Username:{" "}
-					<input
-						type="text"
-						name="username"
-						value={formData.username}
-						onChange={handleInputChange}
-					/>
-				</label>
-				<label>
-					Password:{" "}
-					<input
-						type="text"
-						name="password"
-						value={formData.password}
-						onChange={handleInputChange}
-					/>
-				</label>
-				{isServerError ? (
-					<p className="error">Login failed, incorrect credentials!</p>
-				) : null}
-				<Link to="/register">Click to register!</Link>
-				<button>Login!</button>
-			</form>
-		</div>
+		<Container component={"main"} maxWidth="xs">
+			<br />
+			<br />
+			<SignInForm />
+		</Container>
 	);
 };
 
