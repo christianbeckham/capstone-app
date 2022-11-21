15b541825e21f342e78feaf21106b6014e8e565d
Christian Beckham <urenacb22@gmail.com>
2022-11-21 12:26:31 -0600

    feat: created new form components for login and register

---
+++ frontend/src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,93 @@
+import React, { useContext } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+import AuthContext from "../../context/AuthContext";
+import useCustomForm from "../../hooks/useCustomForm";
+
+const SignUpForm = () => {
+	const { registerUser } = useContext(AuthContext);
+
+	const defaultValues = {
+		username: "",
+		email: "",
+		password: "",
+		firstName: "",
+		lastName: "",
+	};
+
+	const [formData, handleInputChange, handleSubmit] = useCustomForm(
+		defaultValues,
+		registerUser
+	);
+
+	return (
+		<Box
+			component="form"
+			onSubmit={handleSubmit}
+			sx={{ display: "flex", flexDirection: "column" }}
+		>
+			<Typography component={"h1"} variant="h5">
+				Sign Up
+			</Typography>
+			<TextField
+				label="Username"
+				type="text"
+				name="username"
+				value={formData.username}
+				onChange={handleInputChange}
+				variant="standard"
+				margin="normal"
+				required
+				autoFocus
+			/>
+			<TextField
+				label="Email"
+				type="email"
+				name="email"
+				value={formData.email}
+				onChange={handleInputChange}
+				variant="standard"
+				margin="normal"
+				required
+			/>
+			<TextField
+				label="Password"
+				type="password"
+				name="password"
+				value={formData.password}
+				onChange={handleInputChange}
+				variant="standard"
+				margin="normal"
+				required
+			/>
+			<TextField
+				label="First Name"
+				type="text"
+				name="firstName"
+				value={formData.firstName}
+				onChange={handleInputChange}
+				variant="standard"
+				margin="normal"
+				required
+			/>
+			<TextField
+				label="Last Name"
+				type="text"
+				name="lastName"
+				value={formData.lastName}
+				onChange={handleInputChange}
+				variant="standard"
+				margin="normal"
+				required
+			/>
+			<Button type="submit" variant="contained" sx={{ my: 2 }}>
+				Sign up
+			</Button>
+		</Box>
+	);
+};
+
+export default SignUpForm;
