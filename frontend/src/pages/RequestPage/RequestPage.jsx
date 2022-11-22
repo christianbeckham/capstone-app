e740b2d59cf9a1b9a5b7205878952cfb09b154dc
Christian Beckham <urenacb22@gmail.com>
2022-11-22 17:59:45 -0600

    feat: created initial request page

---
+++ frontend/src/pages/RequestPage/RequestPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import DashboardLayout from "../../layouts/DashboardLayout/DashboardLayout";
+
+const RequestPage = () => {
+	return (
+		<DashboardLayout>
+			<h1>Request Page</h1>
+			<p>list of requests...</p>
+		</DashboardLayout>
+	);
+};
+
+export default RequestPage;
