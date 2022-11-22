305da664cefec37e869d669a2efc786e67ddfeea
Christian Beckham <urenacb22@gmail.com>
2022-11-22 09:19:51 -0600

    refactor: cleaned up App since using routes file instead

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,17 @@
 // General Imports
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-
-// Pages Imports
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-
-// Component Imports
+import { AuthProvider } from "./context/AuthContext";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/NavBar/NavBar";
-
-// Util Imports
-import PrivateRoute from "./utils/PrivateRoute";
+import "./App.css";
 
 const App = () => {
 	return (
-		<div>
-			<Navbar />
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<PrivateRoute>
-							<HomePage />
-						</PrivateRoute>
-					}
-				/>
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/login" element={<LoginPage />} />
-			</Routes>
-		</div>
+		<>
+			<AuthProvider>
+				<Navbar />
+				<Outlet />
+			</AuthProvider>
+		</>
 	);
 };
 
