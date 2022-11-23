176da22a60342284cee3bd5b3b2dae5bdc3e0833
Christian Beckham <urenacb22@gmail.com>
2022-11-23 15:22:24 -0600

    feat: created components to render request items

---
+++ frontend/src/components/RequestListItem/RequestListItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
+const RequestListItem = ({request}) => {
+	return (
+		<TableRow
+			key={request.id}
+			sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+		>
+			<TableCell>
+				{new Date(request.created_date).toLocaleDateString()}
+			</TableCell>
+			<TableCell align="left">{request.type}</TableCell>
+			<TableCell align="left">{request.description}</TableCell>
+		</TableRow>
+	);
+};
+
+export default RequestListItem;
