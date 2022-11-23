cd3f5cdd668069c1b66e9e14ab855bb60c88d205
Christian Beckham <urenacb22@gmail.com>
2022-11-23 11:47:35 -0600

    refactor: imported the CheckInImageSerializer and added it to display image info in the API request data

--- backend/check_ins/serializers.py
+++ backend/check_ins/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import CheckIn
+from checkin_images.serializers import CheckInImageSerializer
 
 
 class CheckInSerializer(serializers.ModelSerializer):
+    checkinimage_set = CheckInImageSerializer(many=True, read_only=True)
     class Meta:
         model = CheckIn
         fields = ('user_id', 'id', 'weight', 'feedback', 'created_date', 'checkinimage_set')
