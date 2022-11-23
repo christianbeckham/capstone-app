d0c8358a01a58598ad40647c9755fefc195f4413
Christian Beckham <urenacb22@gmail.com>
2022-11-22 18:12:19 -0600

    feat: updated and added routes

--- frontend/src/routes/router.js
+++ frontend/src/routes/router.js
@@ -4,11 +4,15 @@ import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import HomePage from "../pages/HomePage/HomePage";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
-import DashboardPage from "../pages/DashboardPage/DashboardPage";
 import CheckInPage from "../pages/CheckInPage/CheckInPage";
+import WorkoutPage from "../pages/WorkoutPage/WorkoutPage";
+import RequestPage from "../pages/RequestPage/RequestPage";
+import ExercisePage from "../pages/ExercisePage/ExercisePage";
+
 import MyOverview from "../views/MyOverview/MyOverview";
 import MyCheckIns from "../views/MyCheckIns/MyCheckIns";
 import CheckInForm from "../components/CheckInForm/CheckInForm";
+import MyWorkouts from "../views/MyWorkouts/MyWorkouts";
 
 const router = createBrowserRouter([
 	{
@@ -16,47 +20,25 @@ const router = createBrowserRouter([
 		element: <App />,
 		errorElement: <ErrorPage />,
 		children: [
+			{ path: "/", element: <HomePage /> },
+			{ path: "login", element: <LoginPage /> },
+			{ path: "register", element: <RegisterPage /> },
+			{ path: "dashboard", element: <MyOverview /> },
 			{
-				path: "/",
-				element: <HomePage />,
-			},
-			{
-				path: "login",
-				element: <LoginPage />,
-			},
-			{
-				path: "register",
-				element: <RegisterPage />,
-			},
-			{
-				path: "dashboard",
-				element: <DashboardPage />,
+				path: "checkins",
+				element: <CheckInPage />,
 				children: [
-					{
-						path: "",
-						element: <MyOverview />,
-					},
-					{
-						path: "checkins",
-						element: <CheckInPage />,
-						children: [
-							{
-								path: "",
-								element: <MyCheckIns />,
-							},
-							{ path: "new", element: <CheckInForm /> },
-						],
-					},
-					{
-						path: "workouts",
-						element: <></>,
-					},
-					{
-						path: "requests",
-						element: <></>,
-					},
+					{ path: "", element: <MyCheckIns /> },
+					{ path: "new", element: <CheckInForm /> },
 				],
 			},
+			{
+				path: "workouts",
+				element: <WorkoutPage />,
+				children: [{ path: "", element: <MyWorkouts /> }],
+			},
+			{ path: "exercise/:exerciseId", element: <ExercisePage /> },
+			{ path: "requests", element: <RequestPage /> },
 		],
 	},
 ]);
