ab8516b34073f9e54e5c68f16dc491d7df59f28a
Christian Beckham <urenacb22@gmail.com>
2022-11-23 17:14:59 -0600

    refactor: moved the API logic to the CheckInList component

--- frontend/src/views/MyCheckIns/MyCheckIns.jsx
+++ frontend/src/views/MyCheckIns/MyCheckIns.jsx
@@ -1,41 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Add from "@mui/icons-material/Add";
 
-import useAuth from "../../hooks/useAuth";
 import CheckInForm from "../../components/CheckInForm/CheckInForm";
 import CheckInList from "../../components/CheckInList/CheckInList";
 
 const MyCheckIns = () => {
-	const [user, token] = useAuth();
-	const [checkins, setCheckins] = useState([]);
 	const [showForm, setShowForm] = useState(false);
 
 	const toggleForm = () => setShowForm(!showForm);
-
-	useEffect(() => {
-		const fetchCheckIns = async () => {
-			try {
-				const response = await axios.get(
-					"http://localhost:8000/api/checkins/",
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
-				if (response.status === 200) {
-					const data = await response.data;
-					setCheckins(data);
-				}
-			} catch (e) {
-				console.log({ error: e });
-			}
-		};
-
-		fetchCheckIns();
-	}, []);
 
 	return (
 		<div>
@@ -47,7 +20,7 @@ const MyCheckIns = () => {
 			{showForm && <CheckInForm showForm={showForm} toggleForm={toggleForm} />}
 			<br />
 			<br />
-			<CheckInList checkins={checkins} />
+			<CheckInList />
 		</div>
 	);
 };
