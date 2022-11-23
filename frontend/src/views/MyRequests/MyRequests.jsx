40295fc156dc377824a7b0e48001c63e90786134
Christian Beckham <urenacb22@gmail.com>
2022-11-23 16:09:04 -0600

    feat: created view to render request list items and button to open request form

---
+++ frontend/src/views/MyRequests/MyRequests.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Add from "@mui/icons-material/Add";
+
+import RequestForm from "../../components/RequestForm/RequestForm";
+import RequestList from "../../components/RequestList/RequestList";
+
+const MyRequests = () => {
+	const [showForm, setShowForm] = useState(false);
+
+	const toggleForm = () => setShowForm(!showForm);
+
+	return (
+		<div>
+			<h1>My Requests</h1>
+			<br />
+			<Button variant="outlined" startIcon={<Add />} onClick={toggleForm}>
+				New
+			</Button>
+			{showForm && <RequestForm showForm={showForm} toggleForm={toggleForm} />}
+			<br />
+			<br />
+			<RequestList />
+		</div>
+	);
+};
+
+export default MyRequests;
