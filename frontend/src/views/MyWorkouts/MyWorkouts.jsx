2c658d126f9d7bc74de1fb9542e2a57169374484
Christian Beckham <urenacb22@gmail.com>
2022-11-22 18:05:43 -0600

    feat: created views folder and MyWorkout component to render separate workout sections

---
+++ frontend/src/views/MyWorkouts/MyWorkouts.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import useAuth from "../../hooks/useAuth";
+import WorkoutList from "../../components/WorkoutList/WorkoutList";
+
+const MyWorkouts = () => {
+	const [user, token] = useAuth();
+	const [workouts, setWorkouts] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const workouts = await axios
+					.get("http://localhost:8000/api/plans/", {
+						headers: {
+							Authorization: `Bearer ${token}`,
+						},
+					})
+					.then((res) => {
+						const response = axios.get(
+							`http://localhost:8000/api/workouts/?plan_id=${res.data[0].id}`,
+							{
+								headers: {
+									Authorization: `Bearer ${token}`,
+									"Content-Type": "application/json",
+								},
+								params: { plan_id: 3 },
+							}
+						);
+						return response;
+					});
+
+				if (workouts.status === 200) {
+					const data = await workouts.data;
+					console.log(data);
+					setWorkouts(data);
+				}
+			} catch (e) {
+				console.log({ error: e.message });
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			<h1>My Workouts</h1>
+			<WorkoutList workouts={workouts} />
+		</div>
+	);
+};
+
+export default MyWorkouts;
