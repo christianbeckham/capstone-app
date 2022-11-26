d06950f56ed2a4f54e24efbbde7c74812e9d0689
Christian Beckham <urenacb22@gmail.com>
2022-11-25 18:19:47 -0600

    refactor: removed fragment jsx element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
-// General Imports
-import { AuthProvider } from "./context/AuthContext";
 import { Outlet } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/NavBar/NavBar";
 import "./App.css";
 
 const App = () => {
 	return (
-		<>
-			<AuthProvider>
-				<Navbar />
-				<Outlet />
-			</AuthProvider>
-		</>
+		<AuthProvider>
+			<Navbar />
+			<Outlet />
+		</AuthProvider>
 	);
 };
 
