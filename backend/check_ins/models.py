97d286896bdf05e4803207e72aaa494f737c220e
Christian Beckham <urenacb22@gmail.com>
2022-11-28 17:42:25 -0600

    clean-up: removed unnecessary comment

--- backend/check_ins/models.py
+++ backend/check_ins/models.py
@@ -9,5 +9,3 @@ class CheckIn(models.Model):
     weight = models.DecimalField(max_digits=4, decimal_places=1)
     feedback = models.CharField(max_length=255)
     created_date = models.DateTimeField(auto_now_add=True)
-
-    # need to add image field to upload/store images
