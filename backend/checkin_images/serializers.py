5a1a8b6474bdec447721710443b91ff0278bcf90
Christian Beckham <urenacb22@gmail.com>
2022-11-28 17:35:52 -0600

    refactor: re-added CheckInSerializer to fix error when uploading images

--- backend/checkin_images/serializers.py
+++ backend/checkin_images/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import CheckInImage
+from check_ins.serializers import CheckInSerializer
 
 
 class CheckInImageSerializer(serializers.ModelSerializer):
+    check_in = CheckInSerializer(many=False, read_only=True)
+    
     class Meta:
         model = CheckInImage
         fields = ('check_in', 'title', 'image', 'check_in_id')
