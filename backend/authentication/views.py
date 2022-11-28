99d0241f1204737c60fc3e9db204b8fd7d1e4124
Christian Beckham <urenacb22@gmail.com>
2022-11-28 17:51:03 -0600

    feat: created new client class view to retrieve clients only

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer
+from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer, ClientSerializer
 
 
 # Create your views here.
@@ -17,3 +17,9 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+
+class ClientViewList(viewsets.ModelViewSet):
+    queryset = User.objects.filter(is_client=True)
+    serializer_class = ClientSerializer
+    permission_classes = (IsAdminUser,)
