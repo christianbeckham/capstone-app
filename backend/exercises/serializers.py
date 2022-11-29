c957c4844ea67b83246e916ee38b5120330790a2
Christian Beckham <urenacb22@gmail.com>
2022-11-28 18:19:52 -0600

    refactor: removed workouts field

--- backend/exercises/serializers.py
+++ backend/exercises/serializers.py
@@ -5,5 +5,4 @@ from .models import Exercise
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
-        fields = ('id', 'name', 'sets', 'reps', 'rest_time', 'time_interval', 'workouts')
-        read_only_fields = ['workouts']
+        fields = ('id', 'name', 'sets', 'reps', 'rest_time', 'time_interval')
