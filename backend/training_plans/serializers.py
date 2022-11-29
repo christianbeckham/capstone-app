30ef3f2b03a52cdcd3462f8bdc291a9a62b36a3f
Christian Beckham <urenacb22@gmail.com>
2022-11-28 18:08:23 -0600

    feat: created new WorkoutSerializer to fix circular import and display data when retrieving training plan

--- backend/training_plans/serializers.py
+++ backend/training_plans/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from .models import TrainingPlan
+from workouts.models import Workout
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workout
+        fields = ('id', 'assigned_day', 'week_day')
 
 
 class TrainingPlanSerializer(serializers.ModelSerializer):
+    workouts = WorkoutSerializer(
+        many=True, read_only=True, source='workout_set')
+
     class Meta:
         model = TrainingPlan
-        fields = ('user_id', 'id', 'goal', 'weekly_workout_days', 'cost')
+        fields = ('user_id', 'id', 'goal',
+                  'weekly_workout_days', 'cost', 'workouts')
+        # depth = 1
 
-    user_id = serializers.IntegerField(write_only=True)
+    user_id = serializers.IntegerField(read_only=True)
