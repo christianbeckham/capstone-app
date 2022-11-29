cfa0572aadc204da3ce2da6fb2ec94830331f759
Christian Beckham <urenacb22@gmail.com>
2022-11-28 18:05:28 -0600

    feat: added related_name attribute

--- backend/training_plans/models.py
+++ backend/training_plans/models.py
@@ -6,7 +6,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class TrainingPlan(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='training_plan')
     goal = models.CharField(max_length=255)
     weekly_workout_days = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(7)])
