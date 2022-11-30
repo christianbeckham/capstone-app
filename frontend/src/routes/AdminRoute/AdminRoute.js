dd5174b0a7ab88f24fc8ee8f20a70860a98f3137
Christian Beckham <urenacb22@gmail.com>
2022-11-29 19:42:52 -0600

    feat: updated admin route to use admin layout

--- frontend/src/routes/AdminRoute/AdminRoute.js
+++ frontend/src/routes/AdminRoute/AdminRoute.js
@@ -1,9 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
+import AdminLayout from "../../layouts/AdminLayout/AdminLayout";
+
 const AdminRoute = () => {
 	const [user] = useAuth();
-	return user?.is_admin ? <Outlet /> : <Navigate to="/login" replace />;
+	return user?.is_admin ? (
+		<AdminLayout>
+			<Outlet />
+		</AdminLayout>
+	) : (
+		<Navigate to="/login" replace />
+	);
 };
 
 export default AdminRoute;
