8f9dfa392715fdfde4ccc64fce5fa8426b951488
Christian Beckham <urenacb22@gmail.com>
2022-11-30 14:38:11 -0600

    refactor: removed button from and form reference to include button in the from component

--- frontend/src/views/MyCheckIns/MyCheckIns.jsx
+++ frontend/src/views/MyCheckIns/MyCheckIns.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import Add from "@mui/icons-material/Add";
+import React from "react";
 
 import CheckInForm from "../../components/CheckInForm/CheckInForm";
 import CheckInList from "../../components/CheckInList/CheckInList";
 
 const MyCheckIns = () => {
-	const [showForm, setShowForm] = useState(false);
-
-	const toggleForm = () => setShowForm(!showForm);
-
 	return (
 		<div>
 			<h1>My Check-Ins</h1>
 			<br />
-			<Button variant="outlined" startIcon={<Add />} onClick={toggleForm}>
-				New
-			</Button>
-			{showForm && <CheckInForm showForm={showForm} toggleForm={toggleForm} />}
+			<CheckInForm />
 			<br />
 			<br />
 			<CheckInList />
