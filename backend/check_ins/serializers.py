a62eaaed67e1e49457b66c7cab969b1266fc659a
Christian Beckham <urenacb22@gmail.com>
2022-11-29 19:32:59 -0600

    feat: created new user serializer to avoid circular issue and added it to the CheckInSerializer

--- backend/check_ins/serializers.py
+++ backend/check_ins/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .models import CheckIn
 from checkin_images.models import CheckInImage
+from authentication.models import User
+
+
+class ReadOnlyUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'full_name', 'first_name', 'last_name')
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -12,8 +19,9 @@ class ImageSerializer(serializers.ModelSerializer):
 class CheckInSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True,
                              source='checkinimage_set')
+    user = ReadOnlyUserSerializer(many=False, read_only=True)
 
     class Meta:
         model = CheckIn
-        fields = ('user_id', 'id', 'weight',
-                  'feedback', 'created_date', 'images')
+        fields = ('user', 'id', 'weight',
+                  'weekly_review', 'trainer_feedback', 'created_date', 'images')
