176fadbb398fc94419838f111dad1c838a686b15
Christian Beckham <urenacb22@gmail.com>
2022-11-29 19:36:35 -0600

    feat: created a new status field, added new status method property to get as text, and created new choice class for status.

--- backend/client_requests/models.py
+++ backend/client_requests/models.py
@@ -5,7 +5,18 @@ from authentication.models import User
 
 
 class ClientRequest(models.Model):
+    class StatusChoice(models.IntegerChoices):
+        OPEN = 1
+        CLOSED = 0
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
+    status = models.IntegerField(
+        choices=StatusChoice.choices, default=StatusChoice.OPEN)
+    response = models.CharField(max_length=255, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def status_text(self):
+        return self.StatusChoice(self.status).name.lower()
