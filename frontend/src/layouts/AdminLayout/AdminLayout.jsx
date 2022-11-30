5b34709f6f32e273d691a943dbc7504d36463d2d
Christian Beckham <urenacb22@gmail.com>
2022-11-29 20:21:03 -0600

    feat: created admin and user layouts to have consistency for components render in each route

---
+++ frontend/src/layouts/AdminLayout/AdminLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useOutlet } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import AdminNav from "../../components/AdminNav/AdminNav";
+
+const AdminLayout = () => {
+	const outlet = useOutlet();
+
+	return (
+		<Box sx={{ display: "flex" }}>
+			<CssBaseline />
+			<AdminNav />
+			<Stack component="main" sx={{ flexGrow: 1, p: 3 }} spacing={2}>
+				{outlet}
+			</Stack>
+		</Box>
+	);
+};
+
+export default AdminLayout;
