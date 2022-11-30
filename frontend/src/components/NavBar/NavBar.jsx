be895a7d1159816f4099ecac37c663579c47c235
Christian Beckham <urenacb22@gmail.com>
2022-11-29 19:58:09 -0600

    feat: updated nav bar to have popout menu options

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -9,8 +9,12 @@ import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Avatar from "@mui/material/Avatar";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import WifiTethering from "@mui/icons-material/WifiTethering";
+import Logout from "@mui/icons-material/Logout";
 
 const Navbar = () => {
 	const { logoutUser, user } = useContext(AuthContext);
@@ -44,37 +48,73 @@ const Navbar = () => {
 			}}
 			open={isMenuOpen}
 			onClose={handleMenuClose}
+			PaperProps={{
+				elevation: 0,
+				sx: {
+					overflow: "visible",
+					filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+					// mt: 1.5,
+					"& .MuiAvatar-root": {
+						width: 24,
+						height: 24,
+						ml: -0.5,
+						mr: 1,
+					},
+					"&:before": {
+						content: '""',
+						display: "block",
+						position: "absolute",
+						top: 0,
+						right: 14,
+						width: 10,
+						height: 10,
+						bgcolor: "background.paper",
+						transform: "translateY(-50%) rotate(45deg)",
+						zIndex: 0,
+					},
+				},
+			}}
 		>
-			<MenuItem onClick={() => navigate("dashboard")}>Dashboard</MenuItem>
-			<MenuItem onClick={() => navigate("/")}>Account</MenuItem>
-			<MenuItem onClick={logoutUser}>Logout</MenuItem>
+			<MenuItem onClick={() => navigate("dashboard")}>
+				<Avatar />
+				Dashboard
+			</MenuItem>
+			<Divider />
+			<MenuItem onClick={logoutUser}>
+				<ListItemIcon>
+					<Logout fontSize="small" />
+				</ListItemIcon>
+				Logout
+			</MenuItem>
 		</Menu>
 	);
 
 	return (
 		<Box sx={{ flexGrow: 1, margin: 0 }}>
 			<AppBar position="relative" sx={{ zIndex: 1500 }}>
-				<Toolbar>
-					<WifiTethering sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-					<Typography
-						variant="h6"
-						noWrap
-						component={Link}
-						to="/"
-						sx={{
-							width: 100,
-							mr: 2,
-							display: { xs: "none", md: "flex" },
-							fontFamily: "monospace",
-							fontWeight: 700,
-							letterSpacing: ".3rem",
-							color: "inherit",
-							textDecoration: "none",
-							flexGrow: 1,
-						}}
-					>
-						FitHub
-					</Typography>
+				<Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+					<Box sx={{ display: "flex", alignItems: "center" }}>
+						<WifiTethering
+							sx={{ display: { xs: "none", md: "flex" }, mx: 1 }}
+						/>
+						<Typography
+							variant="h5"
+							noWrap
+							component={Link}
+							to="/"
+							sx={{
+								width: "100%",
+								display: { xs: "none", md: "flex" },
+								color: "inherit",
+								textDecoration: "none",
+								flexGrow: 1,
+								ml: 1,
+								mr: 2,
+							}}
+						>
+							FitHub
+						</Typography>
+					</Box>
 					{user ? (
 						<Chip
 							icon={<AccountCircle />}
