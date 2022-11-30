70180972d8819e1535cd5714de02934f90ea1439
Christian Beckham <urenacb22@gmail.com>
2022-11-29 19:51:34 -0600

    feat: created initial clients view with request and list component

---
+++ frontend/src/views/AdminClients/AdminClients.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Typography from "@mui/material/Typography";
+
+import useAuth from "../../hooks/useAuth";
+import AdminClientList from "../../components/AdminClientList/AdminClientList";
+
+const AdminClients = () => {
+	const [clients, setClients] = useState([]);
+	const [user, token] = useAuth();
+
+	useEffect(() => {
+		try {
+			const fetchClients = async () => {
+				const response = await axios.get("http://localhost:8000/api/clients/", {
+					headers: { Authorization: `Bearer ${token}` },
+				});
+				if (response.status === 200) {
+					setClients(response.data);
+					console.log(response.data);
+				}
+			};
+			fetchClients();
+		} catch (error) {}
+	}, []);
+
+	return (
+		<div>
+			<Typography variant="h5" color="text.primary">
+				All Clients
+			</Typography>
+			<br />
+			{clients.length > 0 ? (
+				<AdminClientList clients={clients} />
+			) : (
+				<p>No clients</p>
+			)}
+		</div>
+	);
+};
+
+export default AdminClients;
