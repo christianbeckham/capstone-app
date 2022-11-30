fe09287b674061b0007db7195ff76bd5f878d3c1
Christian Beckham <urenacb22@gmail.com>
2022-11-29 20:22:50 -0600

    refactor: removed unused imports

--- frontend/src/pages/RequestPage/RequestPage.jsx
+++ frontend/src/pages/RequestPage/RequestPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import DashboardLayout from "../../layouts/DashboardLayout/DashboardLayout";
 
 const RequestPage = () => {
 	return (
