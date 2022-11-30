5baea2c62f428baed4b96b8f9a99f45388b13c62
Christian Beckham <urenacb22@gmail.com>
2022-11-29 19:37:24 -0600

    feat: created new client user serializer and added it to the ClientRequestSerializer to get user info

--- backend/client_requests/serializers.py
+++ backend/client_requests/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import ClientRequest
 
+from authentication.models import User
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email',
+                  'first_name', 'last_name', 'full_name')
+
 
 class ClientRequestSerializer(serializers.ModelSerializer):
+    user = ClientSerializer(read_only=True)
+    status_text = serializers.ReadOnlyField()
+
     class Meta:
         model = ClientRequest
-        fields = ('user_id', 'id', 'type', 'description', 'created_date')
+        fields = ('id', 'type', 'description', 'status',
+                  'response', 'created_date', 'user', 'status_text')
