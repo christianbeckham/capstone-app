48d3b2b710ed618d3bebc09331ddf4c9b1bd0912
Christian Beckham <urenacb22@gmail.com>
2022-12-02 15:00:08 -0600

    feat: updated url patterns

--- backend/exercises/urls.py
+++ backend/exercises/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.exercise_list),
+    path('', views.admin_exercise_list),
     path('<int:pk>/', views.exercise_detail),
-    path('all/', views.api_exercises_list),
-    path('all/<int:pk>/', views.api_exercise_detail),
+    path('db/', views.api_exercises_list),
+    path('db/<int:pk>/', views.api_exercise_detail),
 ]
