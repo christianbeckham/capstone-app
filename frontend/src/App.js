c4b6f92c12068cb40e39613c6727e2071dd556de
Christian Beckham <urenacb22@gmail.com>
2022-12-01 18:03:49 -0600

    refactor: updated NavBar path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Navbar from "./components/NavBar/NavBar";
+import Navbar from "./components/app/NavBar/NavBar";
 import "./App.css";
 
 const App = () => {
