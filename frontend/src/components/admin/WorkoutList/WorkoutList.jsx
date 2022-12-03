52590c231ff1839bf73b1e81faeeb0b682685ca8
Christian Beckham <urenacb22@gmail.com>
2022-12-03 11:06:30 -0600

    feat: updated component to include NewWorkoutForm, EditWorkoutForm, and fetch workouts by training plan id

--- frontend/src/components/admin/WorkoutList/WorkoutList.jsx
+++ frontend/src/components/admin/WorkoutList/WorkoutList.jsx
@@ -1,25 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import Avatar from "@mui/material/Avatar";
 
+import useAuth from "../../../hooks/useAuth";
+import NewWorkoutForm from "../NewWorkoutForm/NewWorkoutForm";
 import EditWorkoutForm from "../EditWorkoutForm/EditWorkoutForm";
 
-const WorkoutList = ({ workouts }) => {
+const WorkoutList = ({ planId }) => {
+	const [user, token] = useAuth();
+	const [workouts, setWorkouts] = useState([]);
+
+	const fetchWorkoutsByPlan = async () => {
+		try {
+			const response = await axios.get(
+				`http://localhost:8000/api/workouts/?plan_id=${planId}`,
+				{
+					headers: { Authorization: `Bearer ${token}` },
+				}
+			);
+			if (response.status === 200) {
+				console.log("workout info", response.data);
+				setWorkouts(response.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		if (planId !== undefined) fetchWorkoutsByPlan();
+	}, [planId]);
+
 	return (
 		<div>
 			<Grid item xs={12} md={6}>
+				<Box>
+					<Typography
+						variant="h5"
+						color="text.primary"
+						sx={{
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							my: 1,
+						}}
+					>
+						Workouts
+						<NewWorkoutForm planId={planId} />
+					</Typography>
+				</Box>
+				<Divider />
 				<List dense>
 					{workouts?.map((w) => (
 						<ListItem
 							key={w.id}
-							secondaryAction={
-								<EditWorkoutForm workoutId={w.id} />
-							}
+							secondaryAction={<EditWorkoutForm workoutId={w.id} />}
 						>
 							<ListItemAvatar>
 								<Avatar>{w.assigned_day}</Avatar>
