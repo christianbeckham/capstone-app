892dd98b0b3e299674b61f8ea264d5a03fb4149d
Christian Beckham <urenacb22@gmail.com>
2022-12-03 17:41:14 -0600

    feat: created button to render list and components with options to edit user info

---
+++ frontend/src/components/admin/EditProfileOptions/EditProfileOptions.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+import { styled, alpha } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+import EditProfileModal from "../EditProfileModal/EditProfileModal";
+import EditPlanModal from "../EditPlanModal/EditPlanModal";
+
+const StyledMenu = styled((props) => (
+	<Menu
+		elevation={0}
+		anchorOrigin={{
+			vertical: "bottom",
+			horizontal: "right",
+		}}
+		transformOrigin={{
+			vertical: "top",
+			horizontal: "right",
+		}}
+		{...props}
+	/>
+))(({ theme }) => ({
+	"& .MuiPaper-root": {
+		borderRadius: 6,
+		marginTop: theme.spacing(0),
+		minWidth: 120,
+		color:
+			theme.palette.mode === "light"
+				? "rgb(55, 65, 81)"
+				: theme.palette.grey[300],
+		boxShadow:
+			"rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+		"& .MuiMenu-list": {
+			padding: "4px 0",
+		},
+		"& .MuiMenuItem-root": {
+			"& .MuiSvgIcon-root": {
+				fontSize: 18,
+				color: theme.palette.text.secondary,
+				marginRight: theme.spacing(1.5),
+			},
+			"&:active": {
+				backgroundColor: alpha(
+					theme.palette.primary.main,
+					theme.palette.action.selectedOpacity
+				),
+			},
+		},
+	},
+}));
+
+const EditProfileOptions = ({ client, fetchClientUser }) => {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<div>
+			<Button
+				id="edit-profile-button"
+				aria-controls={open ? "edit-profile-menu" : undefined}
+				aria-haspopup="true"
+				aria-expanded={open ? "true" : undefined}
+				variant="contained"
+				disableElevation
+				onClick={handleClick}
+				endIcon={<KeyboardArrowDownIcon />}
+			>
+				Edit
+			</Button>
+			<StyledMenu
+				id="edit-profile-menu"
+				MenuListProps={{
+					"aria-labelledby": "demo-customized-button",
+				}}
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				autoFocus={false}
+				keepMounted
+			>
+				<MenuItem
+					component={EditProfileModal}
+					client={client}
+					fetchClientUser={fetchClientUser}
+					handleClose={handleClose}
+					disableRipple
+				/>
+				<MenuItem
+					component={EditPlanModal}
+					plan={client?.training_plan}
+					fetchClientUser={fetchClientUser}
+					handleClose={handleClose}
+					disableRipple
+				/>
+			</StyledMenu>
+		</div>
+	);
+};
+
+export default EditProfileOptions;
