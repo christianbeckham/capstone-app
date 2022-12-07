3299543e102dbc4c046182c9ce058f3471d8b896
Christian Beckham <urenacb22@gmail.com>
2022-12-06 19:02:41 -0600

    refactor: updated single axios get request into two separate requests and useEffects

--- frontend/src/views/MyWorkouts/MyWorkouts.jsx
+++ frontend/src/views/MyWorkouts/MyWorkouts.jsx
@@ -2,46 +2,45 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import useAuth from "../../hooks/useAuth";
-import WorkoutList from "../../components/WorkoutList/WorkoutList";
+import WorkoutList from "../../components/user/WorkoutList/WorkoutList";
 
 const MyWorkouts = () => {
 	const [user, token] = useAuth();
+	const [planId, setPlanId] = useState(null);
 	const [workouts, setWorkouts] = useState([]);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const workouts = await axios
-					.get("http://localhost:8000/api/plans/", {
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					})
-					.then((res) => {
-						const response = axios.get(
-							`http://localhost:8000/api/workouts/?plan_id=${res.data[0].id}`,
-							{
-								headers: {
-									Authorization: `Bearer ${token}`,
-									"Content-Type": "application/json",
-								},
-							}
-						);
-						return response;
-					});
+	const fetchPlan = async () => {
+		try {
+			const response = await axios.get("http://localhost:8000/api/plans/", {
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			if (response.status === 200) setPlanId(response.data[0].id);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-				if (workouts.status === 200) {
-					const data = await workouts.data;
-					console.log(data);
-					setWorkouts(data);
+	const fetchWorkouts = async (itemId) => {
+		try {
+			const response = await axios.get(
+				`http://localhost:8000/api/workouts/?plan_id=${itemId}`,
+				{
+					headers: { Authorization: `Bearer ${token}` },
 				}
-			} catch (e) {
-				console.log({ error: e.message });
-			}
-		};
+			);
+			if (response.status === 200) setWorkouts(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-		fetchData();
+	useEffect(() => {
+		fetchPlan();
 	}, []);
+
+	useEffect(() => {
+		fetchWorkouts(planId);
+	}, [planId]);
 
 	return (
 		<div>
