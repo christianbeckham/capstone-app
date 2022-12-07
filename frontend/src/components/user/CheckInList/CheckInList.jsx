cad60df0e28d49e79b96d6cda41d123b7acddd78
Christian Beckham <urenacb22@gmail.com>
2022-12-06 18:27:52 -0600

    feat: added pagination to table component

--- frontend/src/components/user/CheckInList/CheckInList.jsx
+++ frontend/src/components/user/CheckInList/CheckInList.jsx
@@ -7,14 +7,26 @@ import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
+import TablePagination from "@mui/material/TablePagination";
 import Paper from "@mui/material/Paper";
 
 import useAuth from "../../../hooks/useAuth";
 import CheckInListItem from "../CheckInListItem/CheckInListItem";
 
 const CheckInList = () => {
-	const [checkins, setCheckins] = useState([]);
 	const [user, token] = useAuth();
+	const [checkins, setCheckins] = useState([]);
+	const [page, setPage] = useState(0);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
+
+	const handleChangePage = (e, newPage) => {
+		setPage(newPage);
+	};
+
+	const handleChangeRowsPerPage = (event) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
 
 	useEffect(() => {
 		const fetchCheckIns = async () => {
@@ -42,7 +54,7 @@ const CheckInList = () => {
 	return (
 		<div>
 			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+				<Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
 					<TableHead>
 						<TableRow>
 							<TableCell>Date</TableCell>
@@ -55,9 +67,11 @@ const CheckInList = () => {
 					</TableHead>
 					<TableBody>
 						{checkins.length > 0 ? (
-							checkins.map((checkin) => (
-								<CheckInListItem key={checkin.id} checkin={checkin} />
-							))
+							checkins
+								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+								.map((checkin) => (
+									<CheckInListItem key={checkin.id} checkin={checkin} />
+								))
 						) : (
 							<TableRow scope="row">
 								<TableCell>No entries...</TableCell>
@@ -66,6 +80,15 @@ const CheckInList = () => {
 					</TableBody>
 				</Table>
 			</TableContainer>
+			<TablePagination
+				rowsPerPageOptions={[10, 15, 25]}
+				component="div"
+				count={checkins.length}
+				rowsPerPage={rowsPerPage}
+				page={page}
+				onPageChange={handleChangePage}
+				onRowsPerPageChange={handleChangeRowsPerPage}
+			/>
 		</div>
 	);
 };
