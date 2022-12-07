daad073f6e66302a8dadab531615f72bd98b583b
Christian Beckham <urenacb22@gmail.com>
2022-12-06 18:06:51 -0600

    feat: added new enpoint to retrieve total checkins count

--- backend/check_ins/urls.py
+++ backend/check_ins/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.client_checkins_list),
     path('<int:pk>/', views.checkin_detail),
+    path('total/', views.checkins_total_count),
     path('all/', views.admin_checkins_list),
     path('all/<int:pk>/', views.admin_checkin_detail),
 ]
