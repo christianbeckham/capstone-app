6d76edd0cf1216b95720194c48a854d820a9c8a4
Christian Beckham <urenacb22@gmail.com>
2022-12-10 12:40:39 -0600

    updated hook to return complete AuthContext values

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,8 +2,7 @@ import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 
 const useAuth = () => {
-	const { user, token } = useContext(AuthContext);
-	return [user, token];
+	return useContext(AuthContext);
 };
 
 export default useAuth;
