4025c05dbc602333e5c8735010d8671635f67051
Christian Beckham <urenacb22@gmail.com>
2022-12-09 21:43:38 -0600

    feat: udpated component to adapt to side menu width

--- frontend/src/components/app/PageToolbar/PageToolbar.jsx
+++ frontend/src/components/app/PageToolbar/PageToolbar.jsx
@@ -5,18 +5,21 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
+import useSettings from "../../../hooks/useSettings";
 import AccountMenuButton from "../AccountMenuButton/AccountMenuButton";
 
 const PageToolbar = ({ children, pageTitle }) => {
+	const { collapsed } = useSettings();
+	const width = collapsed ? 100 : 280;
 	return (
 		<Box>
 			<AppBar
 				color="transparent"
 				position="fixed"
-				sx={{ width: `calc(100% - ${280}px)` }}
+				sx={{ width: `calc(100% - ${width}px)` }}
 			>
 				<Toolbar sx={{ height: 100, mx: 3 }}>
-					<Typography variant="h2" component="h1" sx={{ flexGrow: 1 }}>
+					<Typography component="h1" variant="h3" sx={{ flexGrow: 1 }}>
 						{pageTitle}
 					</Typography>
 					{children}
