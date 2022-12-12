7545610d4eeb3da03962f082952aec3d5dc63185
Christian Beckham <urenacb22@gmail.com>
2022-12-12 11:18:51 -0600

    feat: updated useAuth hook destructuring

--- frontend/src/components/app/AccountMenuButton/AccountMenuButton.jsx
+++ frontend/src/components/app/AccountMenuButton/AccountMenuButton.jsx
@@ -1,14 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 
-import AuthContext from "../../../context/AuthContext";
+import useAuth from "../../../hooks/useAuth";
 import AccountMenuList from "../AccountMenuList/AccountMenuList";
 
 const AccountMenuButton = () => {
-	const { user } = useContext(AuthContext);
+	const { user } = useAuth();
 	const [menuAnchor, setMenuAnchor] = useState(null);
 	const navigate = useNavigate();
 
