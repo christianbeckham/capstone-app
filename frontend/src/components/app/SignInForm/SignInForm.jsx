642ffdfd48b5000c81db02ed53dc8c6420c24b63
Christian Beckham <urenacb22@gmail.com>
2022-12-12 11:20:45 -0600

    feat: udpated component layout, styling, and logic to display error message

--- frontend/src/components/app/SignInForm/SignInForm.jsx
+++ frontend/src/components/app/SignInForm/SignInForm.jsx
@@ -1,18 +1,29 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
-import { Link as RouterLink } from "react-router-dom";
+import InputAdornment from "@mui/material/InputAdornment";
+import CircularProgress from "@mui/material/CircularProgress";
 
-import AuthContext from "../../../context/AuthContext";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+import useAuth from "../../../hooks/useAuth";
 import useCustomForm from "../../../hooks/useCustomForm";
 
 const SignInForm = () => {
-	const { loginUser, isServerError } = useContext(AuthContext);
+	const navigate = useNavigate();
+	const { loginUser, isServerError, setIsServerError, isLoading } = useAuth();
+	const [open, setOpen] = useState(false);
+	const [showPassword, setShowPassword] = useState(false);
 
 	const defaultValues = { username: "", password: "" };
 	const [formData, handleInputChange, handleSubmit] = useCustomForm(
@@ -20,54 +31,110 @@ const SignInForm = () => {
 		loginUser
 	);
 
+	const handleClickShowPassword = () => setShowPassword(!showPassword);
+
+	const handleRegisterLink = () => {
+		setIsServerError(false);
+		setOpen(false);
+		navigate("/register");
+	};
+
+	const handleClose = () => {
+		setIsServerError(false);
+		setOpen(false);
+	};
+
+	useEffect(() => {
+		if (isServerError) setOpen(true);
+	}, [isServerError]);
+
 	return (
-		<Box
-			component="form"
-			onSubmit={handleSubmit}
-			sx={{ display: "flex", flexDirection: "column" }}
-		>
-			{isServerError ? (
-				<Alert severity="error">Unable to login.</Alert>
-			) : (
-				<>
-					<br />
-					<br />
-				</>
-			)}
-			<Typography component={"h1"} variant="h5">
-				Sign In
-			</Typography>
-			<TextField
-				label="Username"
-				type="text"
-				name="username"
-				value={formData.username}
-				onChange={handleInputChange}
-				variant="standard"
-				margin="normal"
-				required
-				autoFocus
-			/>
-			<TextField
-				label="Password"
-				type="password"
-				name="password"
-				value={formData.password}
-				onChange={handleInputChange}
-				variant="standard"
-				margin="normal"
-				required
-			/>
-			<Button type="submit" variant="contained" sx={{ my: 2 }}>
-				Sign in
-			</Button>
-			<Grid container display={"flex"} justifyContent="center">
-				<Grid item>
-					<Link component={RouterLink} to="/register" underline="hover">
-						Don't have an account? Sign up
-					</Link>
-				</Grid>
-			</Grid>
+		<Box sx={{ width: "100%" }}>
+			<Snackbar
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				autoHideDuration={5000}
+				open={open}
+				onClose={handleClose}
+			>
+				<Alert
+					severity="error"
+					variant="filled"
+					sx={{ width: "100%" }}
+					onClose={handleClose}
+				>
+					Unable to login.
+				</Alert>
+			</Snackbar>
+			<Box component="form" onSubmit={handleSubmit}>
+				<Typography component={"h1"} variant="h4" gutterBottom>
+					Sign In
+				</Typography>
+				<Stack spacing={2}>
+					<TextField
+						label="Username"
+						type="text"
+						name="username"
+						value={formData.username}
+						onChange={handleInputChange}
+						variant="standard"
+						margin="normal"
+						required
+						autoFocus
+					/>
+					<TextField
+						label="Password"
+						type={showPassword ? "text" : "password"}
+						name="password"
+						value={formData.password}
+						onChange={handleInputChange}
+						variant="standard"
+						margin="normal"
+						required
+						helperText={showPassword ? "Password is visible" : " "}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton
+										aria-label="toggle password visibility"
+										onClick={handleClickShowPassword}
+										size="small"
+									>
+										{showPassword ? (
+											<Visibility fontSize="inherit" />
+										) : (
+											<VisibilityOff fontSize="inherit" />
+										)}
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+				</Stack>
+				<Stack>
+					<Button
+						type="submit"
+						variant="contained"
+						startIcon={
+							isLoading && <CircularProgress color="common" size={20} />
+						}
+						fullWidth
+						sx={{ my: 2 }}
+					>
+						Sign in
+					</Button>
+					<Stack direction="row" justifyContent="center" spacing={1}>
+						<Typography variant="body2">Don’t have an account? {""}</Typography>
+						<Link
+							component="button"
+							onClick={handleRegisterLink}
+							variant="subtitle2"
+							underline="hover"
+						>
+							Sign up
+						</Link>
+					</Stack>
+				</Stack>
+			</Box>
 		</Box>
 	);
 };
