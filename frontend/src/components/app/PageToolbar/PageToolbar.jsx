e20fd41fb79ee8b9491ad8c278fdb90ea336f177
Christian Beckham <urenacb22@gmail.com>
2022-12-12 11:17:37 -0600

    feat: updated to render mobile menu icon and adjust based on nav collapsed state

--- frontend/src/components/app/PageToolbar/PageToolbar.jsx
+++ frontend/src/components/app/PageToolbar/PageToolbar.jsx
@@ -2,27 +2,49 @@ import React from "react";
 
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import Menu from "@mui/icons-material/Menu";
 
 import useSettings from "../../../hooks/useSettings";
 import AccountMenuButton from "../AccountMenuButton/AccountMenuButton";
 
 const PageToolbar = ({ children, pageTitle }) => {
-	const { collapsed } = useSettings();
+	const { collapsed, toggleMobileMenu } = useSettings();
 	const width = collapsed ? 100 : 280;
 	return (
 		<Box>
 			<AppBar
-				color="transparent"
+				color="default"
 				position="fixed"
-				sx={{ width: `calc(100% - ${width}px)` }}
+				sx={{
+					width: { lg: `calc(100% - ${width}px)` },
+					marginLeft: { lg: width },
+				}}
 			>
 				<Toolbar sx={{ height: 100, mx: 3 }}>
-					<Typography component="h1" variant="h3" sx={{ flexGrow: 1 }}>
-						{pageTitle}
-					</Typography>
-					{children}
+					<IconButton
+						color="inherit"
+						aria-label="open mobile menu"
+						edge="start"
+						onClick={toggleMobileMenu}
+						sx={{
+							display: { lg: "none" },
+							marginRight: 2,
+						}}
+					>
+						<Menu />
+					</IconButton>
+					<Stack direction="row" alignItems="center" spacing={2} flexGrow={1}>
+						<Typography component="h1" variant="h3">
+							{pageTitle}
+						</Typography>
+						<Stack direction="row" alignItems="center" spacing={2}>
+							{children}
+						</Stack>
+					</Stack>
 					<AccountMenuButton />
 				</Toolbar>
 			</AppBar>
