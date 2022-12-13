367d3f207511944cd5694f56c20ede0f3e49ee93
Christian Beckham <urenacb22@gmail.com>
2022-12-13 14:34:32 -0600

    refactor: moved SettingsContext to index.js to apply global styles at a higher level and removed from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { SettingsProvider } from "./context/SettingsContext";
 
 const App = () => {
 	return (
 		<AuthProvider>
-			<SettingsProvider>
-				<Outlet />
-			</SettingsProvider>
+			<Outlet />
 		</AuthProvider>
 	);
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { SettingsProvider } from "./context/SettingsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<SettingsProvider>
+			<RouterProvider router={router} />
+		</SettingsProvider>
 	</React.StrictMode>
 );
