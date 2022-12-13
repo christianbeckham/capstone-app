3a79d5e73c3538138655de7e2f31d724d8e678b2
Christian Beckham <urenacb22@gmail.com>
2022-12-13 14:40:35 -0600

    feat: added PageToolbar component to display header with edit button, added InfoRing component to render macro data inside Card component, and changed layout

--- frontend/src/pages/admin/UserProfilePage/UserProfilePage.jsx
+++ frontend/src/pages/admin/UserProfilePage/UserProfilePage.jsx
@@ -3,18 +3,19 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import CircularProgress from "@mui/material/CircularProgress";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
 
 import useAuth from "../../../hooks/useAuth";
+import PageToolbar from "../../../components/app/PageToolbar/PageToolbar";
 import ProfileCard from "../../../components/admin/ProfileCard/ProfileCard";
 import ProfileCheckInList from "../../../components/admin/ProfileCheckInList/ProfileCheckInList";
 import ProfileWorkoutList from "../../../components/admin/ProfileWorkoutList/ProfileWorkoutList";
 import EditProfileOptions from "../../../components/admin/EditProfileOptions/EditProfileOptions";
+import InfoRing from "../../../components/app/InfoRing/InfoRing";
 
 const UserProfilePage = () => {
 	const { token } = useAuth();
@@ -44,189 +45,46 @@ const UserProfilePage = () => {
 
 	return (
 		<div>
-			<Box
-				sx={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "space-between",
-					mb: 2,
-				}}
-			>
-				<Box sx={{ display: "flex", alignItems: "center" }}>
-					<Avatar
-						sx={{ height: 56, width: 56, m: 2 }}
-						variant="circular"
-						src="avatar1.jpg"
-					/>
-					<Typography variant="h4" color="text.primary">
-						{client?.first_name} {client?.last_name}
-					</Typography>
-					<Stack direction={"row"} spacing={2} sx={{ mx: 4 }}>
-						<Box sx={{ position: "relative", display: "flex" }}>
-							<CircularProgress
-								variant="determinate"
-								size="5rem"
-								thickness={3}
-								value={100}
-							/>
-							<Box
-								sx={{
-									top: 0,
-									left: 0,
-									bottom: 0,
-									right: 0,
-									position: "absolute",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<Stack alignItems={"center"}>
-									<Typography
-										variant="body1"
-										component="div"
-										color="text.secondary"
-									>
-										2,400
-									</Typography>
-									<Typography
-										variant="caption"
-										component="div"
-										color="text.secondary"
-									>
-										calories
-									</Typography>
-								</Stack>
-							</Box>
-						</Box>
-						<Box sx={{ position: "relative", display: "flex" }}>
-							<CircularProgress
-								variant="determinate"
-								size="5rem"
-								thickness={3}
-								value={100}
-							/>
-							<Box
-								sx={{
-									top: 0,
-									left: 0,
-									bottom: 0,
-									right: 0,
-									position: "absolute",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<Stack alignItems={"center"}>
-									<Typography
-										variant="body1"
-										component="div"
-										color="text.secondary"
-									>
-										120g
-									</Typography>
-									<Typography
-										variant="caption"
-										component="div"
-										color="text.secondary"
-									>
-										protein
-									</Typography>
-								</Stack>
-							</Box>
-						</Box>
-						<Box sx={{ position: "relative", display: "flex" }}>
-							<CircularProgress
-								variant="determinate"
-								size="5rem"
-								thickness={3}
-								value={100}
-							/>
-							<Box
-								sx={{
-									top: 0,
-									left: 0,
-									bottom: 0,
-									right: 0,
-									position: "absolute",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<Stack alignItems={"center"}>
-									<Typography
-										variant="body1"
-										component="div"
-										color="text.secondary"
-									>
-										185g
-									</Typography>
-									<Typography
-										variant="caption"
-										component="div"
-										color="text.secondary"
-									>
-										carbs
-									</Typography>
-								</Stack>
-							</Box>
-						</Box>
-						<Box sx={{ position: "relative", display: "flex" }}>
-							<CircularProgress
-								variant="determinate"
-								size="5rem"
-								thickness={3}
-								value={100}
-							/>
-							<Box
-								sx={{
-									top: 0,
-									left: 0,
-									bottom: 0,
-									right: 0,
-									position: "absolute",
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "center",
-								}}
-							>
-								<Stack alignItems={"center"}>
-									<Typography
-										variant="body1"
-										component="div"
-										color="text.secondary"
-									>
-										70g
-									</Typography>
-									<Typography
-										variant="caption"
-										component="div"
-										color="text.secondary"
-									>
-										fats
-									</Typography>
-								</Stack>
-							</Box>
-						</Box>
-					</Stack>
-				</Box>
-				<Box>
-					<EditProfileOptions
-						client={client}
-						fetchClientUser={fetchClientUser}
-					/>
-				</Box>
-			</Box>
+			<PageToolbar pageTitle={`${client?.first_name} ${client?.last_name}`}>
+				<EditProfileOptions client={client} fetchClientUser={fetchClientUser} />
+			</PageToolbar>
 			<Divider />
 			<br />
 			<Grid container spacing={2}>
 				<Grid item xs={3}>
-					<ProfileCard client={client} />
+					<ProfileCard client={client} fetchClientUser={fetchClientUser} />
 				</Grid>
 				<Grid item xs={4}>
-					<ProfileCheckInList userId={clientId} />
+					<Grid item container spacing={2}>
+						<Grid item xs={12}>
+							<Card>
+								<CardHeader title={"Macros"} />
+								<CardContent>
+									<Stack direction="row" spacing={1}>
+										<InfoRing
+											text="calories"
+											value={client?.training_plan?.calories}
+										/>
+										<InfoRing
+											text="protein"
+											value={`${client?.training_plan?.protein}g`}
+										/>
+										<InfoRing
+											text="carbs"
+											value={`${client?.training_plan?.carbs}g`}
+										/>
+										<InfoRing
+											text="fats"
+											value={`${client?.training_plan?.fats}g`}
+										/>
+									</Stack>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={12}>
+							<ProfileCheckInList userId={clientId} />
+						</Grid>
+					</Grid>
 				</Grid>
 				<Grid item xs={4}>
 					<ProfileWorkoutList
