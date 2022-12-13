23a755981c0d359912aa995026f226b774f6ab3e
Christian Beckham <urenacb22@gmail.com>
2022-12-12 19:00:21 -0600

    feat: updated table cells and udpated status text to render component & color

--- frontend/src/components/user/RequestListItem/RequestListItem.jsx
+++ frontend/src/components/user/RequestListItem/RequestListItem.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
+import Chip from "@mui/material/Chip";
+
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 
-const RequestListItem = ({request}) => {
+const RequestListItem = ({ request }) => {
 	return (
-		<TableRow
-			key={request.id}
-			sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-		>
+		<TableRow key={request.id}>
 			<TableCell>
 				{new Date(request.created_date).toLocaleDateString()}
 			</TableCell>
-			<TableCell align="left">{request.type}</TableCell>
-			<TableCell align="left">{request.description}</TableCell>
-			<TableCell align="left">{request.status_text}</TableCell>
+			<TableCell>{request.type}</TableCell>
+			<TableCell>{request.description}</TableCell>
+			<TableCell align="center">
+				{request.status_text === "open" ? (
+					<Chip label={request.status_text.toUpperCase()} color="success" />
+				) : (
+					<Chip label={request.status_text.toUpperCase()} color="error" />
+				)}
+			</TableCell>
 		</TableRow>
 	);
 };
