cf5cf2aa36669cb386f753b269cf9a88a780ebe9
Christian Beckham <urenacb22@gmail.com>
2022-12-13 14:44:34 -0600

    feat: added function to call API and retrieve check-ins, and pass as props to re-render when updated

--- frontend/src/pages/user/CheckInsPage/CheckInsPage.jsx
+++ frontend/src/pages/user/CheckInsPage/CheckInsPage.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+import useAuth from "../../../hooks/useAuth";
 import PageToolbar from "../../../components/app/PageToolbar/PageToolbar";
 import CheckInForm from "../../../components/user/CheckInForm/CheckInForm";
 import CheckInList from "../../../components/user/CheckInList/CheckInList";
 
 const CheckInsPage = () => {
+	const { token } = useAuth();
+	const [checkIns, setCheckIns] = useState([]);
+
+	const fetchCheckIns = async () => {
+		try {
+			const response = await axios.get("http://localhost:8000/api/checkins/", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			if (response.status === 200) {
+				const data = await response.data;
+				setCheckIns(data);
+			}
+		} catch (e) {
+			console.log({ error: e });
+		}
+	};
+
+	useEffect(() => {
+		fetchCheckIns();
+	}, []);
+
 	return (
 		<div>
 			<PageToolbar pageTitle="My Check-Ins">
-				<CheckInForm />
+				<CheckInForm fetchCheckIns={fetchCheckIns} />
 			</PageToolbar>
-			<CheckInList />
+			<CheckInList checkIns={checkIns} />
 		</div>
 	);
 };
