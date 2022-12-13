796721911c2bab838e0235e3ca745d7fe897def8
Christian Beckham <urenacb22@gmail.com>
2022-12-13 14:46:51 -0600

    feat: added function to call API and retrieve requests, and passed as props for rendering on udpates

--- frontend/src/pages/user/RequestsPage/RequestsPage.jsx
+++ frontend/src/pages/user/RequestsPage/RequestsPage.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+import useAuth from "../../../hooks/useAuth";
 import PageToolbar from "../../../components/app/PageToolbar/PageToolbar";
 import RequestForm from "../../../components/user/RequestForm/RequestForm";
 import RequestList from "../../../components/user/RequestList/RequestList";
 
 const RequestsPage = () => {
+	const { token } = useAuth();
+	const [requests, setRequests] = useState([]);
+
+	const fetchRequests = async () => {
+		try {
+			const response = await axios.get("http://localhost:8000/api/requests/", {
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			if (response.status === 200) {
+				const data = await response.data;
+				setRequests(data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchRequests();
+	}, []);
+
 	return (
 		<div>
 			<PageToolbar pageTitle="My Requests">
-				<RequestForm />
+				<RequestForm fetchRequests={fetchRequests} />
 			</PageToolbar>
-			<RequestList />
+			<RequestList requests={requests} />
 		</div>
 	);
 };
