6fefe1beac47739b01796eb2ca1ae028b275f311
Christian Beckham <urenacb22@gmail.com>
2022-12-13 15:32:47 -0600

    fix: updated view function save method to exclude saving data as the current user to avoid overriding as admin

--- backend/training_plans/views.py
+++ backend/training_plans/views.py
@@ -30,7 +30,7 @@ def admin_training_plans_list(request):
     elif request.method == 'POST':
         serializer = TrainingPlanSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(user=request.user)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
