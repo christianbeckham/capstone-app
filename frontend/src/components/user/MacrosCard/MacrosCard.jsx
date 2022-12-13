80f0b315531436222d6c599680adeec89f883007
Christian Beckham <urenacb22@gmail.com>
2022-12-13 14:50:36 -0600

    feat: created component to render macro information on user dashboard

---
+++ frontend/src/components/user/MacrosCard/MacrosCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+
+import InfoRing from "../../app/InfoRing/InfoRing";
+
+const MacrosCard = ({ plan }) => {
+	return (
+		<Card>
+			<CardHeader title={"My Macros"} />
+			<CardContent>
+				<Box
+					sx={{
+						display: "grid",
+						gap: 1,
+						gridTemplateColumns: "repeat(4, 1fr)",
+					}}
+				>
+					<InfoRing text={"calories"} value={plan?.calories} />
+					<InfoRing text={"protein"} value={`${plan?.protein}g`} />
+					<InfoRing text={"carbs"} value={`${plan?.carbs}g`} />
+					<InfoRing text={"fats"} value={`${plan?.fats}g`} />
+				</Box>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default MacrosCard;
