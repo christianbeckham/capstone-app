4ebc06f6b666ad4a9700c993b53735bc270b7fc7
Christian Beckham <urenacb22@gmail.com>
2022-12-13 15:34:13 -0600

    updated model field options to not use or allow null values

--- backend/check_ins/models.py
+++ backend/check_ins/models.py
@@ -7,8 +7,8 @@ from authentication.models import User
 class CheckIn(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     weight = models.DecimalField(max_digits=4, decimal_places=1)
-    weekly_review = models.CharField(max_length=255, blank=True, default="")
-    trainer_feedback = models.CharField(max_length=255, blank=True, default="")
+    weekly_review = models.CharField(max_length=255)
+    trainer_feedback = models.CharField(max_length=255, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
