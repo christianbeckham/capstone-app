09b6f4fff843a3e8f8282ec6f325bf543f73660b
Christian Beckham <urenacb22@gmail.com>
2022-12-13 15:25:52 -0600

    refactor: added new fields to track macros and removed weekly workout field

--- backend/training_plans/models.py
+++ backend/training_plans/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from authentication.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -9,6 +8,8 @@ class TrainingPlan(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='training_plan')
     goal = models.CharField(max_length=255)
-    weekly_workout_days = models.PositiveIntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(7)])
     cost = models.DecimalField(max_digits=5, decimal_places=2)
+    calories = models.PositiveSmallIntegerField(default=0)
+    protein = models.PositiveSmallIntegerField(default=0)
+    carbs = models.PositiveSmallIntegerField(default=0)
+    fats = models.PositiveSmallIntegerField(default=0)
