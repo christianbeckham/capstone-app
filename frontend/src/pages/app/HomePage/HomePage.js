3ab7032c0d5b4e61844976f322182ff22ac4aa49
Christian Beckham <urenacb22@gmail.com>
2022-12-16 13:34:17 -0600

    feat: updated homepage layout & styling and added SignInForm component

--- frontend/src/pages/app/HomePage/HomePage.js
+++ frontend/src/pages/app/HomePage/HomePage.js
@@ -1,27 +1,81 @@
 import React from "react";
 
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 
 import PublicNavbar from "../../../components/app/PublicNavbar/PublicNavbar";
-import PublicSignInButton from "../../../components/app/PublicSignInButton/PublicSignInButton";
+import ThemeToggle from "../../../components/app/ThemeToggle/ThemeToggle";
+import SignInForm from "../../../components/app/SignInForm/SignInForm";
 
 const HomePage = () => {
 	return (
 		<>
 			<PublicNavbar>
-				<PublicSignInButton />
+				<ThemeToggle />
 			</PublicNavbar>
-			<Container
-				maxWidth="lg"
+			<Grid
+				container
+				component="main"
+				maxWidth={"xl"}
 				sx={{
 					height: "100vh",
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
+					m: "auto",
 				}}
 			>
-				<h1>Home page</h1>
-			</Container>
+				<Grid
+					item
+					xs={12}
+					sm={4}
+					md={6}
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<Container component="section" maxWidth="md" sx={{ textAlign: "center" }}>
+						<Typography component="h1" variant="h2">
+							Welcome to FitHub
+						</Typography>
+					</Container>
+				</Grid>
+				<Divider
+					orientation="vertical"
+					variant="middle"
+					flexItem
+					sx={{
+						display: { xs: "none", md: "flex" },
+						m: "auto",
+						height: "70%",
+					}}
+				/>
+				<Grid
+					item
+					xs={12}
+					sm={8}
+					md={5}
+					sx={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<Container component="section" maxWidth="sm">
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+							}}
+						>
+							<SignInForm />
+						</Box>
+					</Container>
+				</Grid>
+			</Grid>
 		</>
 	);
 };
