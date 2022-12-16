a0ef476f00624917ea44559cf3f7da2a1f4720d1
Christian Beckham <urenacb22@gmail.com>
2022-12-16 13:24:48 -0600

    feat: updated table component layout

--- frontend/src/components/admin/RequestTable/RequestTable.jsx
+++ frontend/src/components/admin/RequestTable/RequestTable.jsx
@@ -43,47 +43,33 @@ const RequestTable = ({ requests, fetchRequests }) => {
 					</TableHead>
 					<TableBody>
 						{requests.length > 0 ? (
-							requests
-								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-								.map((request) => (
-									<TableRow
-										key={request.id}
-										sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-									>
-										<TableCell align="left">
-											{new Date(request?.created_date).toLocaleDateString()}
-										</TableCell>
-										<TableCell align="left">{request.type}</TableCell>
-										<TableCell align="left">{request.description}</TableCell>
-										<TableCell align="left">{request.user.full_name}</TableCell>
-										<TableCell align="center">
-											{request.status_text === "open" ? (
-												<Chip
-													label={request.status_text.toUpperCase()}
-													color="success"
-												/>
-											) : (
-												<Chip
-													label={request.status_text.toUpperCase()}
-													color="error"
-												/>
-											)}
-										</TableCell>
-										<TableCell align="center">
-											{request.response ? (
-												<CheckCircleOutline color="success" />
-											) : (
-												<RemoveCircleOutline color="warning" />
-											)}
-										</TableCell>
-										<TableCell align="center">
-											<RequestForm
-												requestInfo={request}
-												fetchRequests={fetchRequests}
-											/>
-										</TableCell>
-									</TableRow>
-								))
+							requests.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((request) => (
+								<TableRow key={request.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+									<TableCell align="left">{new Date(request?.created_date).toLocaleDateString()}</TableCell>
+									<TableCell align="left" sx={{ textTransform: "capitalize" }}>
+										{request.type}
+									</TableCell>
+									<TableCell align="left">{request.description}</TableCell>
+									<TableCell align="left">{request.user.full_name}</TableCell>
+									<TableCell align="center">
+										{request.status_text === "open" ? (
+											<Chip label={request.status_text.toUpperCase()} color="success" />
+										) : (
+											<Chip label={request.status_text.toUpperCase()} color="error" />
+										)}
+									</TableCell>
+									<TableCell align="center">
+										{request.response ? (
+											<CheckCircleOutline color="success" />
+										) : (
+											<RemoveCircleOutline color="warning" />
+										)}
+									</TableCell>
+									<TableCell align="center">
+										<RequestForm requestInfo={request} fetchRequests={fetchRequests} />
+									</TableCell>
+								</TableRow>
+							))
 						) : (
 							<TableRow scope="row">
 								<TableCell>No requests</TableCell>
