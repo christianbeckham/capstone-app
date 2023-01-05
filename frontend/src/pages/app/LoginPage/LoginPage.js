fffebb2dccd19654001a342670c4adf2861e8cd5
Christian Beckham <urenacb22@gmail.com>
2023-01-05 12:00:59 -0600

    feat: added theme toggle button to navbar to display on page

--- frontend/src/pages/app/LoginPage/LoginPage.js
+++ frontend/src/pages/app/LoginPage/LoginPage.js
@@ -3,12 +3,15 @@ import React from "react";
 import Container from "@mui/material/Container";
 
 import PublicNavbar from "../../../components/app/PublicNavbar/PublicNavbar";
+import ThemeToggle from "../../../components/app/ThemeToggle/ThemeToggle";
 import SignInForm from "../../../components/app/SignInForm/SignInForm";
 
 const LoginPage = () => {
 	return (
 		<>
-			<PublicNavbar />
+			<PublicNavbar>
+				<ThemeToggle />
+			</PublicNavbar>
 			<Container
 				component={"main"}
 				maxWidth="xs"
