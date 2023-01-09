b85de8369a05d6d16f4a73bf542beb9b4bdf6e31
Christian Beckham <urenacb22@gmail.com>
2023-01-09 16:39:19 -0600

    feat: added button to list to remove image

--- frontend/src/components/user/ImageList/ImageList.jsx
+++ frontend/src/components/user/ImageList/ImageList.jsx
@@ -1,45 +1,41 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
-const ImageList = ({ images }) => {
+const ImageList = ({ images, handleRemoveImage }) => {
 	return (
-		<div>
-			<Typography component={"h3"} variant="h6" sx={{ pt: 3 }}>
-				Images
-			</Typography>
-			<List
-				sx={{
-					width: "100%",
-					maxWidth: 360,
-					bgcolor: "background.paper",
-				}}
-			>
-				{images &&
-					Object.values(images).map((img, index) => (
-						<div key={img.name}>
-							<ListItem>
-								<ListItemAvatar>
-									<Avatar
-										src={URL.createObjectURL(img)}
-										variant="rounded"
-										sx={{ height: "auto" }}
-									/>
-								</ListItemAvatar>
-								<ListItemText primary={img.name} />
-							</ListItem>
-							{images.length !== index && (
-								<Divider variant="inset" component="li" />
-							)}
-						</div>
-					))}
-			</List>
-		</div>
+		<List
+			sx={{
+				width: "100%",
+				maxWidth: 360,
+				bgcolor: "background.paper",
+			}}
+		>
+			{images &&
+				Object.values(images).map((img, index) => (
+					<div key={img.name}>
+						<ListItem
+							secondaryAction={
+								<IconButton edge="end" aria-label="delete" color="error" onClick={() => handleRemoveImage(index)}>
+									<RemoveCircleIcon />
+								</IconButton>
+							}
+						>
+							<ListItemAvatar>
+								<Avatar src={URL.createObjectURL(img)} variant="rounded" sx={{}} />
+							</ListItemAvatar>
+							<ListItemText primary={img.name} />
+						</ListItem>
+						{images.length !== index && <Divider variant="inset" component="li" />}
+					</div>
+				))}
+		</List>
 	);
 };
 
