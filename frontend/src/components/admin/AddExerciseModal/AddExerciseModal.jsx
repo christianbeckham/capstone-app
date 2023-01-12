0c9d9fee80aeb16f64e1e09e1c88811605a8e46c
Christian Beckham <urenacb22@gmail.com>
2023-01-12 15:26:26 -0600

    refactor: updated component to use Dialog instead of Modal to follow MUI guidelines, updated MUI imports, removed props from components to use theme defaults, and updated button order

--- frontend/src/components/admin/AddExerciseModal/AddExerciseModal.jsx
+++ frontend/src/components/admin/AddExerciseModal/AddExerciseModal.jsx
@@ -1,35 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import DialogContentText from "@mui/material/DialogContentText";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import Add from "@mui/icons-material/Add";
-import Alarm from "@mui/icons-material/Alarm";
+import {
+	Box,
+	Stack,
+	Dialog,
+	TextField,
+	InputAdornment,
+	Button,
+	IconButton,
+	FormControl,
+	InputLabel,
+	Select,
+	MenuItem,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	Typography,
+	Divider,
+} from "@mui/material";
+import { Add, Alarm } from "@mui/icons-material";
 
 import useAuth from "../../../hooks/useAuth";
-
-const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 550,
-	bgcolor: "background.paper",
-	borderRadius: "5px",
-	boxShadow: 24,
-	p: 4,
-};
 
 const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 	const { token } = useAuth();
@@ -45,7 +36,6 @@ const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 	const [selectedTarget, setSelectedTarget] = useState("");
 
 	const handleOpen = () => {
-		console.log("Add Workout Modal OPENED. FETCH 3rd Party API!");
 		fetchExerciseDB();
 		setOpen(true);
 	};
@@ -72,13 +62,9 @@ const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 				workouts_id: workoutId,
 				...newExercise,
 			};
-			const response = await axios.post(
-				"http://localhost:8000/api/exercises/",
-				finalData,
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
+			const response = await axios.post(`${process.env.REACT_APP_WEBSITE_URL}/api/exercises/`, finalData, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
 			if (response.status === 201) {
 				fetchExercises();
 				handleClose();
@@ -103,12 +89,9 @@ const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 
 	const fetchExerciseDB = async () => {
 		try {
-			const response = await axios.get(
-				"http://localhost:8000/api/exercises/db/",
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
+			const response = await axios.get(`${process.env.REACT_APP_WEBSITE_URL}/api/exercises/db/`, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
 			if (response.status === 200) setExerciseDB(response.data);
 		} catch (error) {
 			console.log(error);
@@ -118,20 +101,15 @@ const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 	useEffect(() => {
 		const bodyParts = [...new Set(exerciseDB.map((ex) => ex.bodyPart))].sort();
 		setAllBodyParts(bodyParts);
-	}, []);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [exerciseDB]);
 
 	useEffect(() => {
-		console.log("selected body part is", selectedBodyPart);
 		if (selectedBodyPart !== "") {
 			const target = [
-				...new Set(
-					exerciseDB
-						.filter((ex) => ex.bodyPart === selectedBodyPart)
-						.map((ex) => ex.target)
-				),
+				...new Set(exerciseDB.filter((ex) => ex.bodyPart === selectedBodyPart).map((ex) => ex.target)),
 			].sort();
 			setAllTargets(target);
-			console.log("all targets are", target);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selectedBodyPart]);
@@ -139,13 +117,9 @@ const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 	useEffect(() => {
 		if (selectedBodyPart !== "" && selectedTarget !== "") {
 			const finalExercises = exerciseDB
-				.filter(
-					(ex) =>
-						ex.bodyPart === selectedBodyPart && ex.target === selectedTarget
-				)
+				.filter((ex) => ex.bodyPart === selectedBodyPart && ex.target === selectedTarget)
 				.sort();
 			setFilteredExercises(finalExercises);
-			console.log(finalExercises);
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selectedBodyPart, selectedTarget]);
@@ -163,235 +137,177 @@ const AddExerciseModal = ({ workoutId, fetchExercises }) => {
 
 	return (
 		<div>
-			<IconButton
-				size="small"
-				onClick={handleOpen}
-				sx={{ bgcolor: "background.default" }}
-			>
+			<IconButton size="small" onClick={handleOpen} sx={{ bgcolor: "background.default" }}>
 				<Add fontSize="inherit" />
 			</IconButton>
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description"
-			>
-				<Box sx={style}>
-					<h2>New Exercise</h2>
-					<br />
-					<Grid item>
-						<Box sx={{ mb: 4 }}>
-							<DialogContentText color={"text.primary"}>
-								Filter exercises
-							</DialogContentText>
-							<Box sx={{ mt: 2, mb: 4 }}>
-								<Stack direction={"row"} spacing={2}>
-									<FormControl sx={{ width: "100%" }}>
-										<InputLabel id="bodyPartSelect" shrink variant="standard">
-											Body Part
-										</InputLabel>
-										<Select
-											labelId="bodyPartSelect"
-											label="Body Part"
-											name="body_part"
-											value={selectedBodyPart || ""}
-											onChange={handleBodyPartChange}
-											variant="standard"
-											sx={{ textTransform: "capitalize" }}
-										>
-											{allBodyParts.map((option) => (
-												<MenuItem
-													key={option}
-													value={option}
-													sx={{ textTransform: "capitalize" }}
-												>
-													{option}
-												</MenuItem>
-											))}
-										</Select>
-									</FormControl>
-									<FormControl sx={{ width: "100%" }}>
-										<InputLabel id="targetSelect" shrink variant="standard">
-											Target
-										</InputLabel>
-										<Select
-											id="targetSelect"
-											label="Target"
-											name="target"
-											value={selectedTarget || ""}
-											onChange={handleTargetChange}
-											disabled={!Boolean(selectedBodyPart)}
-											variant="standard"
-											sx={{ textTransform: "capitalize" }}
-										>
-											{allTargets.map((option) => (
-												<MenuItem
-													key={option}
-													value={option}
-													sx={{ textTransform: "capitalize" }}
-												>
-													{option}
-												</MenuItem>
-											))}
-										</Select>
-									</FormControl>
-								</Stack>
-								{filteredExercises.length > 0 && (
-									<p>{filteredExercises.length} exercises available</p>
-								)}
-							</Box>
-							<DialogContentText color={"text.primary"}>
-								Add exercise
-							</DialogContentText>
-							<Box sx={{ mt: 2, mb: 4 }}>
+			<Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+				<DialogTitle>Add Exercise</DialogTitle>
+				<Divider />
+				<DialogContent>
+					<Box sx={{ mb: 4 }}>
+						<Typography variant="body1">Filter Exercises</Typography>
+						<Box sx={{ mt: 2, mb: 4 }}>
+							<Stack direction={"row"} spacing={2}>
 								<FormControl sx={{ width: "100%" }}>
-									<InputLabel
-										id="exerciseSelect"
-										shrink
-										variant="standard"
-										required
-									>
-										Exercise
+									<InputLabel id="bodyPartSelect" shrink variant="standard">
+										Body Part
 									</InputLabel>
 									<Select
-										required
-										labelId="exerciseSelect"
-										label="Exercise"
-										name="name"
-										value={newExercise.name || ""}
-										onChange={handleNewExercise}
-										disabled={!Boolean(selectedTarget)}
-										variant="standard"
-										sx={{ mb: 4, textTransform: "capitalize" }}
-										MenuProps={{ PaperProps: { sx: { maxHeight: 200 } } }}
+										labelId="bodyPartSelect"
+										label="Body Part"
+										name="body_part"
+										value={selectedBodyPart || ""}
+										onChange={handleBodyPartChange}
 									>
-										{filteredExercises.map((option, index) => (
-											<MenuItem
-												key={option.id}
-												value={option.name}
-												dense
-												sx={{
-													textTransform: "capitalize",
-													"& .MuiPopover-paper": {
-														height: 80,
-													},
-												}}
-											>
-												{option.name}
+										{allBodyParts.map((option) => (
+											<MenuItem key={option} value={option}>
+												{option}
 											</MenuItem>
 										))}
 									</Select>
 								</FormControl>
-								<Stack direction="row" spacing={2}>
-									<TextField
-										required
-										fullWidth
-										variant="standard"
-										label="Sets"
-										name="sets"
-										value={newExercise.sets || ""}
-										onChange={handleNewExercise}
-										disabled={!Boolean(newExercise.name)}
-										type="number"
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<Add />
-												</InputAdornment>
-											),
-										}}
-										InputLabelProps={{
-											shrink: true,
-										}}
-									/>
-									<TextField
-										fullWidth
-										required
-										variant="standard"
-										type="number"
-										label="Reps"
-										name="reps"
-										value={newExercise.reps || ""}
-										onChange={handleNewExercise}
-										disabled={!Boolean(newExercise.name)}
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<Add />
-												</InputAdornment>
-											),
-										}}
-										InputLabelProps={{
-											shrink: true,
-										}}
-									/>
-									<TextField
-										fullWidth
-										required
-										variant="standard"
-										type="number"
-										label="Rest"
-										name="rest_time"
-										value={newExercise.rest_time || ""}
-										onChange={handleNewExercise}
-										disabled={!Boolean(newExercise.name)}
-										InputProps={{
-											startAdornment: (
-												<InputAdornment position="start">
-													<Alarm />
-												</InputAdornment>
-											),
-										}}
-										InputLabelProps={{
-											shrink: true,
-										}}
-									/>
-									<FormControl sx={{ width: "100%" }}>
-										<InputLabel
-											id="timeIntervalSelect"
-											shrink
-											variant="standard"
-											required
-										>
-											Time Interval
-										</InputLabel>
-										<Select
-											required
-											labelId="timeIntervalSelect"
-											variant="standard"
-											label="Time Interval"
-											name="time_interval"
-											value={newExercise.time_interval || ""}
-											onChange={handleNewExercise}
-											disabled={!Boolean(newExercise.name)}
-										>
-											{["seconds", "minutes"].map((option) => (
-												<MenuItem key={option} value={option}>
-													{option}
-												</MenuItem>
-											))}
-										</Select>
-									</FormControl>
-								</Stack>
-							</Box>
+								<FormControl sx={{ width: "100%" }}>
+									<InputLabel id="targetSelect" shrink variant="standard">
+										Target
+									</InputLabel>
+									<Select
+										id="targetSelect"
+										label="Target"
+										name="target"
+										value={selectedTarget || ""}
+										onChange={handleTargetChange}
+										disabled={!Boolean(selectedBodyPart)}
+									>
+										{allTargets.map((option) => (
+											<MenuItem key={option} value={option}>
+												{option}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
+							</Stack>
+							{filteredExercises.length > 0 && <p>{filteredExercises.length} exercises available</p>}
 						</Box>
-						<Box sx={{ float: "right" }}>
-							<Stack direction="row" spacing={1}>
-								<Button
-									onClick={addExercise}
-									variant="contained"
-									disabled={!validExercise}
+						<Typography variant="body1">Add Exercise</Typography>
+						<Box sx={{ mt: 2, mb: 4 }}>
+							<FormControl sx={{ width: "100%" }}>
+								<InputLabel id="exerciseSelect" shrink variant="standard" required>
+									Exercise
+								</InputLabel>
+								<Select
+									required
+									labelId="exerciseSelect"
+									label="Exercise"
+									name="name"
+									value={newExercise.name || ""}
+									onChange={handleNewExercise}
+									disabled={!Boolean(selectedTarget)}
+									sx={{ mb: 4 }}
+									MenuProps={{ PaperProps: { sx: { maxHeight: 200 } } }}
 								>
-									Add
-								</Button>
-								<Button onClick={handleClose} variant="contained" color="error">
-									Cancel
-								</Button>
+									{filteredExercises.map((option, index) => (
+										<MenuItem
+											key={option.id}
+											value={option.name}
+											dense
+											sx={{
+												"& .MuiPopover-paper": {
+													height: 80,
+												},
+											}}
+										>
+											{option.name}
+										</MenuItem>
+									))}
+								</Select>
+							</FormControl>
+							<Stack direction="row" spacing={2}>
+								<TextField
+									required
+									fullWidth
+									label="Sets"
+									name="sets"
+									value={newExercise.sets || ""}
+									onChange={handleNewExercise}
+									disabled={!Boolean(newExercise.name)}
+									type="number"
+									InputProps={{
+										startAdornment: (
+											<InputAdornment position="start">
+												<Add />
+											</InputAdornment>
+										),
+									}}
+								/>
+								<TextField
+									fullWidth
+									required
+									type="number"
+									label="Reps"
+									name="reps"
+									value={newExercise.reps || ""}
+									onChange={handleNewExercise}
+									disabled={!Boolean(newExercise.name)}
+									InputProps={{
+										startAdornment: (
+											<InputAdornment position="start">
+												<Add />
+											</InputAdornment>
+										),
+									}}
+								/>
+								<TextField
+									fullWidth
+									required
+									type="number"
+									label="Rest"
+									name="rest_time"
+									value={newExercise.rest_time || ""}
+									onChange={handleNewExercise}
+									disabled={!Boolean(newExercise.name)}
+									InputProps={{
+										startAdornment: (
+											<InputAdornment position="start">
+												<Alarm />
+											</InputAdornment>
+										),
+									}}
+								/>
+								<FormControl sx={{ width: "100%" }}>
+									<InputLabel id="timeIntervalSelect" shrink variant="standard" required>
+										Time Interval
+									</InputLabel>
+									<Select
+										required
+										labelId="timeIntervalSelect"
+										label="Time Interval"
+										name="time_interval"
+										value={newExercise.time_interval || ""}
+										onChange={handleNewExercise}
+										disabled={!Boolean(newExercise.name)}
+									>
+										{["seconds", "minutes"].map((option) => (
+											<MenuItem key={option} value={option}>
+												{option}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
 							</Stack>
 						</Box>
-					</Grid>
-				</Box>
-			</Modal>
+					</Box>
+					<Box sx={{ float: "right" }}></Box>
+				</DialogContent>
+				<Divider />
+				<DialogActions sx={{ mb: 1, mx: 2 }}>
+					<Button onClick={handleClose} variant="outlined" color="error">
+						Cancel
+					</Button>
+					<Button onClick={addExercise} disabled={!validExercise} color="success">
+						Add
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</div>
 	);
 };
