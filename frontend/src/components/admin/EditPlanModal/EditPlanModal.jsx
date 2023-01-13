64345fd780a716c1c29d18c08387bd704dfd2a55
Christian Beckham <urenacb22@gmail.com>
2023-01-13 09:09:43 -0600

    feat: updated component to use Dialog instead of Modal as suggested by MUI, updated API urls to use env variable, updated TextField props, and updated MUI import statement

--- frontend/src/components/admin/EditPlanModal/EditPlanModal.jsx
+++ frontend/src/components/admin/EditPlanModal/EditPlanModal.jsx
@@ -1,125 +1,100 @@
 import React, { useState, forwardRef } from "react";
 import axios from "axios";
-
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Divider from "@mui/material/Divider";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
+import {
+	Box,
+	Stack,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	TextField,
+	InputAdornment,
+	Button,
+	IconButton,
+	MenuItem,
+	Typography,
+	Divider,
+} from "@mui/material";
 import Delete from "@mui/icons-material/Delete";
 
 import useAuth from "../../../hooks/useAuth";
 
-const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 500,
-	bgcolor: "background.paper",
-	borderRadius: "5px",
-	boxShadow: 24,
-	p: 4,
-};
+const EditPlanModal = forwardRef(({ plan, fetchClientUser, handleClose }, ref) => {
+	const { token } = useAuth();
+	const [open, setOpen] = useState(false);
+	const [formData, setFormData] = useState({});
 
-const EditPlanModal = forwardRef(
-	({ plan, fetchClientUser, handleClose }, ref) => {
-		const { token } = useAuth();
-		const [open, setOpen] = useState(false);
-		const [formData, setFormData] = useState({});
+	const handleOpen = () => {
+		handleClose();
+		setOpen(true);
+	};
 
-		const handleOpen = () => {
-			handleClose();
-			setOpen(true);
-		};
+	const handleModalClose = () => {
+		setFormData({
+			goal: plan?.goal,
+			cost: plan?.cost,
+		});
+		setOpen(false);
+	};
 
-		const handleModalClose = () => {
-			setFormData({
-				goal: plan?.goal,
-				cost: plan?.cost,
+	const handleFieldChange = (e) => {
+		e.persist();
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		updatePlan(formData);
+		setOpen(false);
+	};
+
+	const handleDelete = () => {
+		deletePlan();
+		handleModalClose();
+	};
+
+	const updatePlan = async (data) => {
+		try {
+			const response = await axios.patch(`${process.env.REACT_APP_WEBSITE_URL}/api/plans/${plan?.id}/`, data, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
 			});
-			setOpen(false);
-		};
-
-		const handleFieldChange = (e) => {
-			e.persist();
-			setFormData((prevState) => ({
-				...prevState,
-				[e.target.name]: e.target.value,
-			}));
-		};
-
-		const handleSubmit = (e) => {
-			e.preventDefault();
-			updatePlan(formData);
-			setOpen(false);
-		};
-
-		const handleDelete = () => {
-			deletePlan();
-			handleModalClose();
-		};
-
-		const updatePlan = async (data) => {
-			try {
-				const response = await axios.patch(
-					`http://localhost:8000/api/plans/${plan?.id}/`,
-					data,
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
-				if (response.status === 200) {
-					console.log(response.data);
-					fetchClientUser();
-				}
-			} catch (error) {
-				console.log(error);
+			if (response.status === 200) {
+				console.log(response.data);
+				fetchClientUser();
 			}
-		};
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-		const deletePlan = async () => {
-			try {
-				const response = await axios.delete(
-					`http://localhost:8000/api/plans/${plan?.id}/`,
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
-				if (response.status === 204) {
-					console.log(response.data);
-					fetchClientUser();
-				}
-			} catch (error) {
-				console.log(error);
+	const deletePlan = async () => {
+		try {
+			const response = await axios.delete(`${process.env.REACT_APP_WEBSITE_URL}/api/plans/${plan?.id}/`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			if (response.status === 204) {
+				console.log(response.data);
+				fetchClientUser();
 			}
-		};
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-		return (
-			<div>
-				<MenuItem onClick={handleOpen}>Plan</MenuItem>
-				<Modal
-					open={open}
-					onClose={handleModalClose}
-					aria-labelledby="modal-modal-title"
-					aria-describedby="modal-modal-description"
-				>
-					<Box component={"form"} onSubmit={handleSubmit} sx={style}>
-						<Stack
-							direction="row"
-							alignItems="center"
-							justifyContent="space-between"
-							sx={{ mb: 1 }}
-						>
+	return (
+		<div>
+			<MenuItem onClick={handleOpen}>Plan</MenuItem>
+			<Dialog open={open} onClose={handleModalClose} maxWidth="sm" fullWidth>
+				<Box component={"form"} onSubmit={handleSubmit}>
+					<DialogTitle>
+						<Stack direction="row" alignItems="center" justifyContent="space-between">
 							<Typography component="h2" variant="h5">
 								Edit Plan
 							</Typography>
@@ -127,9 +102,10 @@ const EditPlanModal = forwardRef(
 								<Delete fontSize="inherit" />
 							</IconButton>
 						</Stack>
-
-						<Divider />
-						<Stack rowGap={2} sx={{ my: 4 }}>
+					</DialogTitle>
+					<Divider />
+					<DialogContent>
+						<Stack rowGap={2}>
 							<TextField
 								label="Goal"
 								type="text"
@@ -138,10 +114,6 @@ const EditPlanModal = forwardRef(
 								value={formData.goal || plan?.goal}
 								onChange={handleFieldChange}
 								rows={4}
-								variant="standard"
-								InputLabelProps={{
-									shrink: true,
-								}}
 							/>
 							<TextField
 								label="Cost"
@@ -149,36 +121,26 @@ const EditPlanModal = forwardRef(
 								value={formData.cost || plan?.cost}
 								onChange={handleFieldChange}
 								sx={{ width: 100 }}
-								InputLabelProps={{
-									shrink: true,
-								}}
 								InputProps={{
 									inputMode: "numeric",
 									pattern: "[0-9]*",
-									startAdornment: (
-										<InputAdornment position="start">$</InputAdornment>
-									),
+									startAdornment: <InputAdornment position="start">$</InputAdornment>,
 								}}
-								variant="standard"
 							/>
 						</Stack>
-						<Stack direction="row" spacing={1} sx={{ float: "right" }}>
-							<Button type="submit" variant="contained" color="success">
-								Save
-							</Button>
-							<Button
-								variant="contained"
-								color="error"
-								onClick={handleModalClose}
-							>
-								Cancel
-							</Button>
-						</Stack>
-					</Box>
-				</Modal>
-			</div>
-		);
-	}
-);
+					</DialogContent>
+					<DialogActions>
+						<Button variant="outlined" color="error" onClick={handleModalClose}>
+							Cancel
+						</Button>
+						<Button type="submit" color="success">
+							Save
+						</Button>
+					</DialogActions>
+				</Box>
+			</Dialog>
+		</div>
+	);
+});
 
 export default EditPlanModal;
