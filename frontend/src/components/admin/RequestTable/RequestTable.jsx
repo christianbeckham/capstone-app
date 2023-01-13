86276cf263d23a59248a2cd5892a88978522ead2
Christian Beckham <urenacb22@gmail.com>
2023-01-13 16:43:58 -0600

    feat: updated conditional rendering, page count options, and empty rows implementation to fix content jumping when moving pages

--- frontend/src/components/admin/RequestTable/RequestTable.jsx
+++ frontend/src/components/admin/RequestTable/RequestTable.jsx
@@ -15,7 +15,7 @@ import RequestForm from "../RequestForm/RequestForm";
 
 const RequestTable = ({ requests, fetchRequests }) => {
 	const [page, setPage] = useState(0);
-	const [rowsPerPage, setRowsPerPage] = useState(5);
+	const [rowsPerPage, setRowsPerPage] = useState(10);
 
 	const handleChangePage = (e, newPage) => {
 		setPage(newPage);
@@ -26,68 +26,82 @@ const RequestTable = ({ requests, fetchRequests }) => {
 		setPage(0);
 	};
 
+	const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - requests.length) : 0;
+
 	return (
-		<div>
-			<TableContainer>
-				<Table aria-label="requests table">
-					<TableHead>
-						<TableRow>
-							<TableCell>Date</TableCell>
-							<TableCell>Request Type</TableCell>
-							<TableCell>Description</TableCell>
-							<TableCell>By</TableCell>
-							<TableCell align="center">Status</TableCell>
-							<TableCell align="center">Responded</TableCell>
-							<TableCell></TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{requests.length > 0 ? (
-							requests.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((request) => (
-								<TableRow key={request.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-									<TableCell align="left">{new Date(request?.created_date).toLocaleDateString()}</TableCell>
-									<TableCell align="left" sx={{ textTransform: "capitalize" }}>
-										{request.type}
-									</TableCell>
-									<TableCell align="left">{request.description}</TableCell>
-									<TableCell align="left">{request.user.full_name}</TableCell>
-									<TableCell align="center">
-										{request.status_text === "open" ? (
-											<Chip label={request.status_text.toUpperCase()} color="success" />
-										) : (
-											<Chip label={request.status_text.toUpperCase()} color="error" />
-										)}
-									</TableCell>
-									<TableCell align="center">
-										{request.response ? (
-											<CheckCircleOutline color="success" />
-										) : (
-											<RemoveCircleOutline color="warning" />
-										)}
-									</TableCell>
-									<TableCell align="center">
-										<RequestForm requestInfo={request} fetchRequests={fetchRequests} />
-									</TableCell>
+		<>
+			{requests.length > 0 ? (
+				<>
+					<TableContainer>
+						<Table aria-label="requests table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Date</TableCell>
+									<TableCell>Request Type</TableCell>
+									<TableCell>Description</TableCell>
+									<TableCell>By</TableCell>
+									<TableCell align="center">Status</TableCell>
+									<TableCell align="center">Responded</TableCell>
+									<TableCell></TableCell>
 								</TableRow>
-							))
-						) : (
-							<TableRow scope="row">
-								<TableCell>No requests</TableCell>
-							</TableRow>
-						)}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<TablePagination
-				rowsPerPageOptions={[5, 10, 15]}
-				component="div"
-				count={requests.length}
-				rowsPerPage={rowsPerPage}
-				page={page}
-				onPageChange={handleChangePage}
-				onRowsPerPageChange={handleChangeRowsPerPage}
-			/>
-		</div>
+							</TableHead>
+							<TableBody>
+								{requests.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((request) => (
+									<TableRow key={request.id} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+										<TableCell align="left">{new Date(request?.created_date).toLocaleDateString()}</TableCell>
+										<TableCell align="left" sx={{ textTransform: "capitalize" }}>
+											{request.type}
+										</TableCell>
+										<TableCell
+											align="left"
+											sx={{ maxWidth: "300px", textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden" }}
+										>
+											{request.description}
+										</TableCell>
+										<TableCell align="left">{request.user.full_name}</TableCell>
+										<TableCell align="center">
+											{request.status_text === "open" ? (
+												<Chip label={request.status_text.toUpperCase()} color="success" />
+											) : (
+												<Chip label={request.status_text.toUpperCase()} color="error" />
+											)}
+										</TableCell>
+										<TableCell align="center">
+											{request.response ? (
+												<CheckCircleOutline color="success" />
+											) : (
+												<RemoveCircleOutline color="warning" />
+											)}
+										</TableCell>
+										<TableCell align="center">
+											<RequestForm requestInfo={request} fetchRequests={fetchRequests} />
+										</TableCell>
+									</TableRow>
+								))}
+								{emptyRows > 0 && (
+									<TableRow sx={{ height: 55 * emptyRows, "& td": { backgroundColor: "transparent" } }}>
+										<TableCell colSpan={6} />
+									</TableRow>
+								)}
+							</TableBody>
+						</Table>
+					</TableContainer>
+					<TablePagination
+						rowsPerPageOptions={[10, 15, 25]}
+						component="div"
+						count={requests.length}
+						rowsPerPage={rowsPerPage}
+						page={page}
+						onPageChange={handleChangePage}
+						onRowsPerPageChange={handleChangeRowsPerPage}
+					/>
+				</>
+			) : (
+				<TableRow scope="row">
+					<TableCell>No requests</TableCell>
+				</TableRow>
+			)}
+		</>
 	);
 };
 
