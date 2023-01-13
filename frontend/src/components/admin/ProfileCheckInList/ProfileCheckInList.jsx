07cb8d4c4e1b3a47663aa9275ec19075a11636ba
Christian Beckham <urenacb22@gmail.com>
2023-01-13 17:51:20 -0600

    feat: updated component to uset MUI Table components, added more data to display image count, and added empty rows logic to prevent jumping when switching pages

--- frontend/src/components/admin/ProfileCheckInList/ProfileCheckInList.jsx
+++ frontend/src/components/admin/ProfileCheckInList/ProfileCheckInList.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import Pagination from "@mui/material/Pagination";
-import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import ExitToApp from "@mui/icons-material/ExitToApp";
+import {
+	Box,
+	Stack,
+	Typography,
+	Pagination,
+	IconButton,
+	TableContainer,
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+} from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 import useAuth from "../../../hooks/useAuth";
 
 const ProfileCheckInList = ({ userId }) => {
 	const { token } = useAuth();
 	const [checkins, setCheckins] = useState([]);
-
 	const [page, setPage] = useState(1);
 	// eslint-disable-next-line no-unused-vars
 	const [rowsPerPage, setRowsPerPage] = useState(5);
@@ -27,14 +31,10 @@ const ProfileCheckInList = ({ userId }) => {
 
 	const fetchUserCheckIns = async () => {
 		try {
-			const response = await axios.get(
-				`http://localhost:8000/api/checkins/all/?user_id=${userId}`,
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
+			const response = await axios.get(`${process.env.REACT_APP_WEBSITE_URL}/api/checkins/all/?user_id=${userId}`, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
 			if (response.status === 200) {
-				console.log("Profile CheckIns List", response.data);
 				setCheckins(response.data);
 			}
 		} catch (error) {
@@ -43,72 +43,69 @@ const ProfileCheckInList = ({ userId }) => {
 	};
 
 	useEffect(() => {
-		fetchUserCheckIns();
+		if (userId !== undefined) fetchUserCheckIns();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [userId]);
+
+	const emptyRows = page > 1 ? Math.max(0, page * rowsPerPage - checkins?.length) : 0;
 
 	return (
-		<div>
-			<Typography variant="h5" color="text.primary" sx={{ my: 1 }}>
-				Check-Ins
-			</Typography>
-			<Divider />
-			{checkins.length > 0 ? (
-				checkins
-					.slice(
-						(page - 1) * rowsPerPage,
-						(page - 1) * rowsPerPage + rowsPerPage
-					)
-					.map((c) => (
-						<Paper key={c.id} sx={{ p: 1, my: 1 }}>
-							<Box
-								sx={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "space-between",
-								}}
-							>
-								<p>{new Date(c?.created_date).toLocaleDateString()}</p>
-								<IconButton
-									component={Link}
-									to={`/a/checkins/${c.id}`}
-									aria-label="view check-in"
-									color="primary"
-									size="small"
-								>
-									<ExitToApp fontSize="inherit" />
-								</IconButton>
-							</Box>
-						</Paper>
-					))
+		<>
+			{checkins?.length > 0 ? (
+				<>
+					<TableContainer>
+						<Table aria-label="check-ins table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Date</TableCell>
+									<TableCell>Images</TableCell>
+									<TableCell>View</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{checkins.slice((page - 1) * rowsPerPage, (page - 1) * rowsPerPage + rowsPerPage).map((c) => (
+									<TableRow key={c.id}>
+										<TableCell>{new Date(c?.created_date).toLocaleDateString()}</TableCell>
+										<TableCell>{c.images.length}</TableCell>
+										<TableCell>
+											<IconButton
+												component={Link}
+												to={`/a/checkins/${c.id}`}
+												aria-label="view check-in"
+												color="primary"
+												size="small"
+											>
+												<ExitToApp fontSize="inherit" />
+											</IconButton>
+										</TableCell>
+									</TableRow>
+								))}
+								{emptyRows > 0 && (
+									<TableRow sx={{ height: 43 * emptyRows, "& td": { backgroundColor: "transparent" } }}>
+										<TableCell colSpan={6} />
+									</TableRow>
+								)}
+							</TableBody>
+						</Table>
+					</TableContainer>
+					<Stack direction="row" alignItems={"center"} justifyContent="space-between">
+						<Typography variant="caption" component="div" color="text.secondary" sx={{ mx: 2 }}>
+							Total: {checkins.length}
+						</Typography>
+						<Pagination
+							size="small"
+							count={Math.ceil(checkins.length / rowsPerPage)}
+							page={page}
+							onChange={handleChangePage}
+						/>
+					</Stack>
+				</>
 			) : (
 				<Box sx={{ m: 1 }}>
 					<Typography>No check-ins available</Typography>
 				</Box>
 			)}
-			{checkins.length > 0 && (
-				<Stack
-					direction="row"
-					alignItems={"center"}
-					justifyContent="space-between"
-				>
-					<Typography
-						variant="caption"
-						component="div"
-						color="text.secondary"
-						sx={{ mx: 2 }}
-					>
-						Total: {checkins.length}
-					</Typography>
-					<Pagination
-						size="small"
-						count={Math.ceil(checkins.length / rowsPerPage)}
-						page={page}
-						onChange={handleChangePage}
-					/>
-				</Stack>
-			)}
-		</div>
+		</>
 	);
 };
 
