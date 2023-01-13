4b3e53d77e97980ac3ae8673ecf536f4184920e4
Christian Beckham <urenacb22@gmail.com>
2023-01-13 13:55:27 -0600

    update: changed component to use Dialog instead of Modal, updated action buttons, and updated styling/layout

--- frontend/src/components/admin/EditProfileModal/EditProfileModal.jsx
+++ frontend/src/components/admin/EditProfileModal/EditProfileModal.jsx
@@ -1,165 +1,126 @@
 import React, { useEffect, useState, forwardRef } from "react";
 import axios from "axios";
-
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Divider from "@mui/material/Divider";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Switch from "@mui/material/Switch";
+import {
+	Box,
+	Stack,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	TextField,
+	Button,
+	MenuItem,
+	FormGroup,
+	FormControlLabel,
+	Switch,
+	Divider,
+} from "@mui/material";
 
 import useAuth from "../../../hooks/useAuth";
 
-const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: 500,
-	bgcolor: "background.paper",
-	borderRadius: "5px",
-	boxShadow: 24,
-	p: 4,
-};
+const EditProfileModal = forwardRef(({ client, fetchClientUser, handleClose }, ref) => {
+	const { token } = useAuth();
+	const [open, setOpen] = useState(false);
+	const [formData, setFormData] = useState({});
 
-const EditProfileModal = forwardRef(
-	({ client, fetchClientUser, handleClose }, ref) => {
-		const { token } = useAuth();
-		const [open, setOpen] = useState(false);
-		const [formData, setFormData] = useState({});
+	const handleOpen = () => {
+		handleClose();
+		setOpen(true);
+	};
 
-		const handleOpen = () => {
-			handleClose();
-			setOpen(true);
-		};
+	const handleModalClose = () => {
+		setFormData({
+			username: client?.username,
+			email: client?.email,
+			first_name: client?.first_name,
+			last_name: client?.last_name,
+			is_active: client?.is_active,
+		});
+		setOpen(false);
+	};
 
-		const handleModalClose = () => {
-			setFormData({
-				username: client?.username,
-				email: client?.email,
-				first_name: client?.first_name,
-				last_name: client?.last_name,
-				is_active: client?.is_active,
+	const handleFieldChange = (e) => {
+		e.persist();
+		if (e.target.type === "checkbox") {
+			setFormData((prevState) => ({
+				...prevState,
+				[e.target.name]: e.target.checked,
+			}));
+		} else {
+			setFormData((prevState) => ({
+				...prevState,
+				[e.target.name]: e.target.value,
+			}));
+		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		postUpdate(formData);
+		setOpen(false);
+	};
+
+	const postUpdate = async (data) => {
+		try {
+			const response = await axios.patch(`${process.env.REACT_APP_WEBSITE_URL}/api/auth/clients/${client?.id}/`, data, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
 			});
-			setOpen(false);
-		};
-
-		const handleFieldChange = (e) => {
-			e.persist();
-			if (e.target.type === "checkbox") {
-				setFormData((prevState) => ({
-					...prevState,
-					[e.target.name]: e.target.checked,
-				}));
-			} else {
-				setFormData((prevState) => ({
-					...prevState,
-					[e.target.name]: e.target.value,
-				}));
+			if (response.status === 200) {
+				console.log(response.data);
+				fetchClientUser();
 			}
-		};
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-		const handleSubmit = (e) => {
-			e.preventDefault();
-			postUpdate(formData);
-			setOpen(false);
-		};
+	useEffect(() => {
+		setFormData({
+			username: client?.username,
+			email: client?.email,
+			first_name: client?.first_name,
+			last_name: client?.last_name,
+			is_active: client?.is_active,
+		});
+	}, [client]);
 
-		const postUpdate = async (data) => {
-			try {
-				const response = await axios.patch(
-					`http://localhost:8000/api/auth/clients/${client?.id}/`,
-					data,
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
-				if (response.status === 200) {
-					console.log(response.data);
-					fetchClientUser();
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
-
-		useEffect(() => {
-			setFormData({
-				username: client?.username,
-				email: client?.email,
-				first_name: client?.first_name,
-				last_name: client?.last_name,
-				is_active: client?.is_active,
-			});
-		}, [client]);
-
-		return (
-			<div>
-				<MenuItem onClick={handleOpen}>Profile</MenuItem>
-				<Modal
-					open={open}
-					onClose={handleModalClose}
-					aria-labelledby="modal-modal-title"
-					aria-describedby="modal-modal-description"
-				>
-					<Box component={"form"} onSubmit={handleSubmit} sx={style}>
-						<h2>Edit Profile</h2>
-						<Divider />
-						<Stack rowGap={2} sx={{ my: 4 }}>
+	return (
+		<div>
+			<MenuItem onClick={handleOpen}>Profile</MenuItem>
+			<Dialog open={open} onClose={handleModalClose} maxWidth="sm" fullWidth>
+				<Box component={"form"} onSubmit={handleSubmit}>
+					<DialogTitle>Edit Profile</DialogTitle>
+					<Divider />
+					<DialogContent>
+						<Stack rowGap={2}>
 							<Stack direction={"row"} columnGap={2}>
 								<TextField
-									variant="standard"
 									label="First name"
 									type="text"
 									name="first_name"
 									value={formData?.first_name}
 									onChange={handleFieldChange}
-									InputLabelProps={{
-										shrink: true,
-									}}
 									sx={{ width: "100%" }}
 								/>
 								<TextField
-									variant="standard"
 									label="Last name"
 									type="text"
 									name="last_name"
 									value={formData?.last_name}
 									onChange={handleFieldChange}
-									InputLabelProps={{
-										shrink: true,
-									}}
 									sx={{ width: "100%" }}
 								/>
 							</Stack>
 							<TextField
-								variant="standard"
 								label="Username"
 								type="text"
 								name="username"
 								value={formData?.username}
 								onChange={handleFieldChange}
-								InputLabelProps={{
-									shrink: true,
-								}}
 							/>
-							<TextField
-								variant="standard"
-								label="Email"
-								type="email"
-								name="email"
-								value={formData?.email}
-								onChange={handleFieldChange}
-								InputLabelProps={{
-									shrink: true,
-								}}
-							/>
+							<TextField label="Email" type="email" name="email" value={formData?.email} onChange={handleFieldChange} />
 							<FormGroup row>
 								<FormControlLabel
 									control={
@@ -177,23 +138,19 @@ const EditProfileModal = forwardRef(
 								/>
 							</FormGroup>
 						</Stack>
-						<Stack direction="row" spacing={1} sx={{ float: "right" }}>
-							<Button type="submit" variant="contained" color="success">
-								Save
-							</Button>
-							<Button
-								variant="contained"
-								color="error"
-								onClick={handleModalClose}
-							>
-								Cancel
-							</Button>
-						</Stack>
-					</Box>
-				</Modal>
-			</div>
-		);
-	}
-);
+					</DialogContent>
+					<DialogActions>
+						<Button variant="outlined" color="error" onClick={handleModalClose}>
+							Cancel
+						</Button>
+						<Button type="submit" color="success">
+							Save
+						</Button>
+					</DialogActions>
+				</Box>
+			</Dialog>
+		</div>
+	);
+});
 
 export default EditProfileModal;
