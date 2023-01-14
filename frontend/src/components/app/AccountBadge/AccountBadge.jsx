32778c3a23e73eed3d6d1140654a03f85beb3a0a
Christian Beckham <urenacb22@gmail.com>
2023-01-13 19:34:50 -0600

    style: udpated minor styling for better spacing

--- frontend/src/components/app/AccountBadge/AccountBadge.jsx
+++ frontend/src/components/app/AccountBadge/AccountBadge.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Avatar from "@mui/material/Avatar";
@@ -14,7 +13,7 @@ const AccountBadge = () => {
 
 	const fetchAccount = async () => {
 		try {
-			const response = await axios.get("http://localhost:8000/api/auth/me/", {
+			const response = await axios.get(`${process.env.REACT_APP_WEBSITE_URL}/api/auth/me/`, {
 				headers: { Authorization: `Bearer ${token}` },
 			});
 			if (response.status === 200) {
@@ -27,21 +26,24 @@ const AccountBadge = () => {
 
 	useEffect(() => {
 		fetchAccount();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	return (
-		<Box sx={{ m: 2 }}>
+		<Box sx={{ mx: 2 }}>
 			<Stack
 				direction="row"
 				alignItems="center"
+				justifyContent="center"
+				spacing={2}
 				sx={{
-					p: 2,
+					py: 2,
 					backgroundColor: "action.disabledBackground",
-					borderRadius: 2,
+					borderRadius: 1,
 				}}
 			>
 				<Avatar alt="profile avatar" sx={{ width: 24, height: 24 }} />
-				<Box sx={{ ml: 2 }}>
+				<Box>
 					<Typography variant="subtitle2" sx={{ color: "text.primary" }}>
 						{account?.full_name}
 					</Typography>
