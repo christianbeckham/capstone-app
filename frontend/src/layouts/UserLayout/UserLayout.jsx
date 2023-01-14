644f71f2071f5e527e92d301367aa181375491b9
Christian Beckham <urenacb22@gmail.com>
2023-01-14 15:14:51 -0600

    update: removed unused component and added height to page area

--- frontend/src/layouts/UserLayout/UserLayout.jsx
+++ frontend/src/layouts/UserLayout/UserLayout.jsx
@@ -1,29 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { useOutlet } from "react-router-dom";
 
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 
 import SideNav from "../../components/app/SideNav/SideNav";
-import SettingsPanel from "../../components/app/SettingsPanel/SettingsPanel";
 import { userNavItems } from "../../routes/sideNavItems";
 
 const UserLayout = () => {
 	const outlet = useOutlet();
-	const [openSettings, setOpenSettings] = useState(false);
-
-	const handleSettingsToggle = () => setOpenSettings(!openSettings);
 
 	return (
 		<Box sx={{ display: "flex" }}>
 			<SideNav navItems={userNavItems} />
-			<Stack component="main" sx={{ flexGrow: 1, px: 6 }}>
+			<Stack component="main" sx={{ flexGrow: 1, px: 6, height: "100vh" }}>
 				{outlet}
 			</Stack>
-			<SettingsPanel
-				open={openSettings}
-				toggleSettingsPanel={handleSettingsToggle}
-			/>
 		</Box>
 	);
 };
