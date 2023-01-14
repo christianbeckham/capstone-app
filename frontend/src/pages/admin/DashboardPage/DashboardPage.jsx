283202a4c15c1f76f8fa6b13b1e0d8956068169b
Christian Beckham <urenacb22@gmail.com>
2023-01-14 16:43:44 -0600

    feat: updated API urls to use env variable, updated the conditional formatting, updated check-ins list to use Table component, and added loading state/progress

--- frontend/src/pages/admin/DashboardPage/DashboardPage.jsx
+++ frontend/src/pages/admin/DashboardPage/DashboardPage.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
+import {
+	Grid,
+	Box,
+	Stack,
+	Card,
+	CardHeader,
+	CardContent,
+	Typography,
+	Button,
+	IconButton,
+	CircularProgress,
+	Table,
+	TableBody,
+	TableRow,
+	TableCell,
+} from "@mui/material";
 import ExitToApp from "@mui/icons-material/ExitToApp";
 
 import useAuth from "../../../hooks/useAuth";
@@ -22,15 +27,13 @@ const DashboardPage = () => {
 	const { token } = useAuth();
 	const [totalClients, setTotalClients] = useState({});
 	const [latestCheckins, setLatestCheckins] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	const fetchClients = async () => {
 		try {
-			const response = await axios.get(
-				"http://localhost:8000/api/auth/clients/total/",
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
+			const response = await axios.get(`${process.env.REACT_APP_WEBSITE_URL}/api/auth/clients/total/`, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
 			if (response.status === 200) {
 				setTotalClients(response.data);
 			}
@@ -41,15 +44,12 @@ const DashboardPage = () => {
 
 	const fetchLatestCheckins = async () => {
 		try {
-			const response = await axios.get(
-				"http://localhost:8000/api/checkins/all/?limit=5",
-				{
-					headers: { Authorization: `Bearer ${token}` },
-				}
-			);
+			const response = await axios.get(`${process.env.REACT_APP_WEBSITE_URL}/api/checkins/all/?limit=5`, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
 			if (response.status === 200) {
-				console.log("latest checkins", response.data);
 				setLatestCheckins(response.data);
+				setLoading(false);
 			}
 		} catch (error) {
 			console.log(error);
@@ -70,11 +70,7 @@ const DashboardPage = () => {
 					<Card>
 						<CardHeader title={"Client Overview"} />
 						<CardContent>
-							<Stack
-								direction="row"
-								alignItems={"center"}
-								justifyContent={"space-around"}
-							>
+							<Stack direction="row" alignItems={"center"} justifyContent={"space-around"}>
 								<InfoRing text={"Total"} value={totalClients?.total} />
 								<InfoRing text={"Active"} value={totalClients?.active} />
 								<InfoRing text={"Inactive"} value={totalClients?.inactive} />
@@ -84,59 +80,49 @@ const DashboardPage = () => {
 				</Grid>
 				<Grid item xs={8}>
 					<Card>
-						<Stack
-							direction="row"
-							alignItems={"center"}
-							justifyContent={"space-between"}
-						>
-							<CardHeader title={"Latest Check-Ins"} />
-							<Button
-								component={Link}
-								to={"checkins"}
-								sx={{ display: "flex", alignItems: "center" }}
-							>
-								All
-							</Button>
-						</Stack>
-						<CardContent>
-							{latestCheckins.length > 0 ? (
-								latestCheckins.map((c) => (
-									<Paper
-										key={c.id}
-										sx={{ my: 1.5, py: 1, px: 2, width: "100%" }}
-									>
-										<Stack
-											direction="row"
-											columnGap={1}
-											alignItems={"center"}
-											justifyContent="space-between"
-										>
-											<Box>
-												<Typography component="h3" variant="body1">
-													By {c.user.full_name}
-												</Typography>
-												<Typography variant="caption" color="text.secondary">
-													on {new Date(c?.created_date).toLocaleDateString()}
-												</Typography>
-											</Box>
-											<IconButton
-												component={Link}
-												to={`checkins/${c.id}`}
-												aria-label="view check-in"
-												color="primary"
-												sx={{ display: "inline-flex" }}
-											>
-												<ExitToApp />
-											</IconButton>
-										</Stack>
-									</Paper>
-								))
-							) : (
-								<Box sx={{ m: 1 }}>
-									<Typography>No check-ins available</Typography>
+						{loading ? (
+							<Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
+								<CircularProgress disableShrink />
+							</Box>
+						) : (
+							<>
+								<Stack direction="row" alignItems={"center"} justifyContent={"space-between"}>
+									<CardHeader title={"Latest Check-Ins"} />
+									<Button component={Link} to={"checkins"} size="small" sx={{ display: "flex", alignItems: "center" }}>
+										All
+									</Button>
+								</Stack>
+								<Box>
+									{latestCheckins.length > 0 ? (
+										<Table>
+											<TableBody>
+												{latestCheckins.map((c) => (
+													<TableRow key={c.id} hover={true}>
+														<TableCell>{new Date(c?.created_date).toLocaleDateString()}</TableCell>
+														<TableCell sx={{ width: "100%" }}>{c.user.full_name}</TableCell>
+														<TableCell>
+															<IconButton
+																component={Link}
+																to={`checkins/${c.id}`}
+																aria-label="view check-in"
+																color="primary"
+																sx={{ display: "inline-flex" }}
+															>
+																<ExitToApp />
+															</IconButton>
+														</TableCell>
+													</TableRow>
+												))}
+											</TableBody>
+										</Table>
+									) : (
+										<Box sx={{ m: 1 }}>
+											<Typography>No check-ins available</Typography>
+										</Box>
+									)}
 								</Box>
-							)}
-						</CardContent>
+							</>
+						)}
 					</Card>
 				</Grid>
 			</Grid>
