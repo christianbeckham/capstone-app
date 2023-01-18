17e6d5b92ea10d1c408972a341d0c0194ea9555c
Christian Beckham <urenacb22@gmail.com>
2023-01-18 09:25:49 -0600

    style: updated background color to adjust with theme

--- frontend/src/components/app/TableSkeleton/TableSkeleton.jsx
+++ frontend/src/components/app/TableSkeleton/TableSkeleton.jsx
@@ -4,10 +4,20 @@ import { Skeleton } from "@mui/material";
 const TableSkeleton = () => {
 	return (
 		<>
-			<Skeleton sx={{ bgcolor: "grey.200", borderRadius: 1, mb: 1 }} variant="rectangular" width={"100%"} height={40} />
-			<Skeleton sx={{ bgcolor: "grey.200", borderRadius: 1 }} variant="rectangular" width={"100%"} height={300} />
 			<Skeleton
-				sx={{ bgcolor: "grey.200", borderRadius: 1, mt: 1, float: "right" }}
+				sx={{ bgcolor: "background.neutral", borderRadius: 1, mb: 1 }}
+				variant="rectangular"
+				width={"100%"}
+				height={40}
+			/>
+			<Skeleton
+				sx={{ bgcolor: "background.neutral", borderRadius: 1 }}
+				variant="rectangular"
+				width={"100%"}
+				height={300}
+			/>
+			<Skeleton
+				sx={{ bgcolor: "background.neutral", borderRadius: 1, mt: 1, float: "right" }}
 				variant="rectangular"
 				width={"25%"}
 				height={40}
